572a1b939ae87a9212d532d406055ee4b497fa2d
jiaxi <yujx_jessy@outlook.com>
2020-04-24 13:24:50 +0200

    volumn expression corrected

--- master/raw_scripts/chi2_latest/uniform_check&ELGnum.py
+++ master/raw_scripts/chi2_latest/uniform_check&ELGnum.py
@@ -1,4 +1,4 @@
-import matplotlib 
+import matplotl/ib 
 matplotlib.use('agg')
 import time
 import numpy as np
@@ -34,30 +34,43 @@ plt.savefig('uniform-trans_validate.png')
 area = np.array([177.5,240,237.5,131.9])
 areadeg = np.array([417.5,369.4])
 
-z=np.linspace(0.6,1.1,21)
+
 c=299792.458
 Om=0.31
-dfin = c/100/np.sqrt(Om*(1+0.6)**3+1-Om)
-dint = c/100/np.sqrt(Om*(1+1.1)**3+1-Om)
-V = 4/3*np.pi*(dfin**3-dint**3)*np.sum(areadeg)/36000
+z1 = np.linspace(0,1.1,201)
+z2 = np.linspace(0,0.6,201)
+dfin = np.trapz(c/100/np.sqrt(Om*(1+z1)**3+1-Om),dx = z1[1]-z1[0])
+dint = np.trapz(c/100/np.sqrt(Om*(1+z2)**3+1-Om),dx = z2[1]-z2[0])
+allsky = 4*np.pi*(180/np.pi)**2
+V = 4/3*np.pi*(dfin**3-dint**3)*areadeg/allsky
 
-for i,GC in enumerate(['NGC','SGC']):
+for i,GC in enumerate(['SGC','NGC']):
 	hdu = fits.open('/media/jiaxi/disk/Master/obs/eBOSS_ELG_clustering_'+GC+'_v7.dat.fits')[1].data
-	galnum=np.sum(hdu['WEIGHT_SYSTOT']*hdu['WEIGHT_CP']*hdu['WEIGHT_NOZ']*hdu['WEIGHT_FKP'])
+	galnum=np.sum(hdu['WEIGHT_SYSTOT']*hdu['WEIGHT_CP']*hdu['WEIGHT_NOZ'])# no *hdu['WEIGHT_FKP']
 	print(GC+' have {:2.7} galaxies'.format(galnum))
-	print('its number density is {:2.7} gal/(Mpc)**3'.format(galnum/V))
+	print('Volumn is {:2.4} Mpc**3'.format(V[i]))
+	print('its number density is {:2.7} gal/(Mpc)**3 \n'.format(galnum/V[i]))
 
 ## results:
 '''
-NGC have 45367.24 galaxies
-its number density is 8.526405e-05 gal/(Mpc)**3
-SGC have 44901.41 galaxies
-its number density is 8.438856e-05 gal/(Mpc)**3
-difference is within 1%
+SGC have 102464.6 galaxies
+Volumn is 4.789e+08 Mpc**3
+its number density is 0.0002139777 gal/(Mpc)**3 
+
+NGC have 98595.65 galaxies
+Volumn is 4.237e+08 Mpc**3
+its number density is 0.000232783 gal/(Mpc)**3 
+
+their number density difference is around 9%
+
+
+
+
+
 
 UNIT ELG numbers
-NGC:85264.05
-SGC:84388.56
+SGC:349256
+NGC:410223
 
 '''
 
