d08f537fac81686e8a26bf4b37f7f05de2f8c61b
Jiaxi-Yu <yujx_jessy@outlook.com>
2020-04-25 23:44:28 +0200

    ELG obs: ascii.no_header

--- master/raw_scripts/chi2_latest/obs.py
+++ master/raw_scripts/chi2_latest/obs.py
@@ -22,7 +22,7 @@ def obs(home,gal,GC,obsname,randname,obsout,rmin,rmax,nbins,zmin,zmax,Om,exist):
                 obs0 = np.trapz(mon, dx=1./200, axis=1)
                 obs1 = np.trapz(qua, dx=1./200, axis=1)
                 obs2 = np.trapz(hexad, dx=1./200, axis=1)
-                Table([np.linspace(0,250,251)[:-1],np.linspace(0,250,251)[1:],obs0,obs1,obs2]).write(obsout,format = 'ascii',delimiter='\t',overwrite=True)
+                Table([np.linspace(0,250,251)[:-1],np.linspace(0,250,251)[1:],obs0,obs1,obs2]).write(obsout,format = 'ascii.no_header',delimiter='\t',overwrite=True)
 	else:
                 print('The observation 2pcf file already exists.')
 
