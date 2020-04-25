383a639ed59c6933b599d8b161eaab07876aa875
Jiaxi-Yu <yujx_jessy@outlook.com>
2020-04-25 04:29:54 +0200

    observation calculation

--- master/raw_scripts/chi2_latest/obs.py
+++ master/raw_scripts/chi2_latest/obs.py
@@ -5,18 +5,26 @@ from astropy.io import fits
 import os
 import glob
   
-def obs(home,GC,obsname,randname,rmin,rmax,nbins,zmin,zmax,Om,exist):
+def obs(home,gal,GC,obsname,randname,obsout,rmin,rmax,nbins,zmin,zmax,Om,exist):
     # observation 2pcf calculation in interactive node
     # obs has converted to .dat files
-	if exist==False:
+	if (gal==LRG)&(exist==False):
 		print('observation 2pcf calculation:') 
-		print('./FCFC/2pcf -c ./FCFC/fcfc.conf -j 1 -a '+str(rmin)+' -b '+str(rmax)+' -n '+str(nbins)+' --data-z-min '+str(zmin)+' --data-z-max '+str(zmax)+' --rand-z-min '+str(zmin)+' --rand-z-max '+str(zmax)+' -m '+str(Om)+' --data '+home+'catalog/'+obsname[:-5]+' --rand '+home+'catalog/'+randname[:-5]+' --dd '+home+'2PCF/obs/LRG_'+GC+'.dd --dr '+home+'2PCF/obs/LRG_'+GC+'.dr --rr '+home+'2PCF/obs/LRG_'+GC+'.rr --output '+home+'2PCF/obs/LRG_'+GC+'.dat')
-		os.system('./FCFC/2pcf -c ./FCFC/fcfc.conf -j 1 -a '+str(rmin)+' -b '+str(rmax)+' -n '+str(nbins)+' --data-z-min '+str(zmin)+' --data-z-max '+str(zmax)+' --rand-z-min '+str(zmin)+' --rand-z-max '+str(zmax)+' -m '+str(Om)+' --data '+home+'catalog/'+obsname[:-5]+' --rand '+home+'catalog/'+randname[:-5]+' --dd '+home+'2PCF/obs/LRG_'+GC+'.dd --dr '+home+'2PCF/obs/LRG_'+GC+'.dr --rr '+home+'2PCF/obs/LRG_'+GC+'.rr --output '+home+'2PCF/obs/LRG_'+GC+'.dat')
-	else:
+		print('./FCFC/2pcf -c ./FCFC/fcfc.conf -j 1 -a '+str(rmin)+' -b '+str(rmax)+' -n '+str(nbins)+' --data-z-min '+str(zmin)+' --data-z-max '+str(zmax)+' --rand-z-min '+str(zmin)+' --rand-z-max '+str(zmax)+' -m '+str(Om)+' --data '+obsname[:-5]+' --rand '+randname[:-5]+' --dd '+home+'2PCF/obs/LRG_'+GC+'.dd --dr '+home+'2PCF/obs/LRG_'+GC+'.dr --rr '+home+'2PCF/obs/LRG_'+GC+'.rr --output '+home+'2PCF/obs/LRG_'+G)
+		os.system('./FCFC/2pcf -c ./FCFC/fcfc.conf -j 1 -a '+str(rmin)+' -b '+str(rmax)+' -n '+str(nbins)+' --data-z-min '+str(zmin)+' --data-z-max '+str(zmax)+' --rand-z-min '+str(zmin)+' --rand-z-max '+str(zmax)+' -m '+str(Om)+' --data '+obsname[:-5]+' --rand '+randname[:-5]+' --dd '+home+'2PCF/obs/LRG_'+GC+'.dd --dr '+home+'2PCF/obs/LRG_'+GC+'.dr --rr '+home+'2PCF/obs/LRG_'+GC+'.rr --output '+home+'2PCF/obs/LRG_'+GC+'.dat')
+	elif (gal==ELG)&(exist==False):
+                obspc = ascii.read(obsname,format = 'no_header')  # obs pair counts
+                mu = np.linspace(0,1,200)
+		mon = ((obspc['col3']-2*obspc['col4']+obspc['col5'])/obspc['col5']).reshape(250,200)
+		qua = mon * 2.5 * (3 * mu**2 - 1)
+		hexad = mon * 1.125 * (35 * mu**4 - 30 * mu**2 + 3)
+		## use trapz to integrate over mu
+		obs0 = np.trapz(mon, dx=1./200, axis=1)
+		obs1 = np.trapz(qua, dx=1./200, axis=1)
+		obs2 = np.trapz(hexad, dx=1./200, axis=1)
+		Table([obs0[rmin:rmax],obs1[rmin:rmax],obs2[rmin:rmax]],names=['mono','quad','hexa']).write(obsout,format = 'ascii',delimiter='\t',overwrite=True)
+        
+        else:
 		print('The observation 2pcf file already exists.')
 
-#LRG = fits.open(home+'catalog/'+obsname)
-#z   = LRG[1].data['Z'][(LRG[1].data['Z']>zmin)&(LRG[1].data['Z']<zmax)]
-#LRG.close()
 
-#return z.shape[0]
