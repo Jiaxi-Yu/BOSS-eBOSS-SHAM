96af95cafb09798e19535a033ca216d7dbbcf761
jiaxi <yujx_jessy@outlook.com>
2020-09-09 22:36:20 +0200

    until 09 Sep 2020 wrong 2PCF calculations

--- master/raw_scripts/prototypes/covmatrix.py
+++ master/raw_scripts/prototypes/covmatrix.py
@@ -1,5 +1,6 @@
 # calculate the covariance of the mock 2pcf 
 # save both the 1000 mock 2pcf and its covariance
+# and calculate the 2pcf of observations
 import time
 time_start=time.time()
 import numpy as np
@@ -10,65 +11,94 @@ import os
 import glob
 
 # variables(for functions)
-home = '/global/cscratch1/sd/jiaxi/master/'
-rmin=0
-rmax=200
-nbins=200
-GC = 'NGC'    ## 'NGC' 'SGC'
+def covmatrix(home,mockdir,mockfits,gal,GC,zmin,zmax,Om,exist):
+	print('new matrix calculation')
+	if exist ==True:
+		print('The mock covariance file already exists.')
+	else:
+		nbins=200
+		nmu=120
 
-# read dd , dr and rr files
-ddpath = glob.glob('/global/cscratch1/sd/zhaoc/EZmock/2PCF/LRGv7_syst/z0.6z1.0/2PCF/*LRG_'+GC+'*.dd')
-nbins=200
-nmu=120
+		# read dd , dr and rr files
+		ddpath = glob.glob(mockdir+'*'+gal+'_'+GC+'*.dd')
+		nfile = len(ddpath)
+		# read all the 2pcf data
+		mockmono = [x for x in range(len(ddpath))]
+		mockquadru = [x for x in range(len(ddpath))]
+		mockhexadeca = [x for x in range(len(ddpath))]		
+		dd,dr,rr = np.zeros((nfile,nbins*nmu)),np.zeros((nfile,nbins*nmu)),np.zeros((nfile,nbins*nmu))
+		# read the dd,dr,rr of all 1000 mocks
+		for i in range(nfile):
+			# read pair-counts files:
+			dd[i] = ascii.read(ddpath[i],format='no_header')['col6']
+			dr[i] = ascii.read(ddpath[i][:-2]+'dr',format='no_header')['col6']
+			rr[i] = ascii.read(ddpath[i][:-2]+'rr',format='no_header')['col6']
+			if (i/nfile*100%10==0):
+				print(gal+'_'+GC+'_mock ',np.ceil(i/nfile*100),'% 2pcf completed.')
+			if (i==nfile-1):
+				print(gal+'_'+GC+'_mock ',np.ceil(i/nfile*100),'% 2pcf completed.')
+				mu =((ascii.read(ddpath[i],format='no_header')['col1']+ascii.read(ddpath[i],format='no_header')['col2'])/2).reshape(nbins,nmu)
+				s = ((ascii.read(ddpath[i],format='no_header')['col3']+ascii.read(ddpath[i],format='no_header')['col4'])/2).reshape(nbins,nmu)[:,0]
 
-# read all the 2pcf data
-mockmono = [x for x in range(len(ddpath))]
-mockquadru = [x for x in range(len(ddpath))]
-mockhexadeca = [x for x in range(len(ddpath))]
-# calculate 2pcf multipoles and to form a nbins*nfiles matrix
-for i in range(len(ddpath)):
-	# read pair-counts files:
-	dd = ascii.read(ddpath[i],format='no_header')
-	dr = ascii.read(ddpath[i][:-2]+'dr',format='no_header')
-	rr = ascii.read(ddpath[i][:-2]+'rr',format='no_header')
-	# calculate the 2pcf(mu,s)
-	mu = (dd['col1']+dd['col2'])/2
-	mono = (dd['col6']-2*dr['col6']+rr['col6'])/rr['col6']
-	quad = mono * 2.5 * (3 * mu**2 - 1)
-	hexa = mono * 1.125 * (35 * mu**4 - 30 * mu**2 + 3)
-	# integral 2pcf with mu to have 2pcf(s)
-	xi0 = np.trapz(mono.reshape(nbins,nmu), dx=1./nmu, axis=1)
-	xi2 = np.trapz(quad.reshape(nbins,nmu), dx=1./nmu, axis=1)
-	xi4 = np.trapz(hexa.reshape(nbins,nmu), dx=1./nmu, axis=1) 
-	mockmono[i] = xi0
-	mockquadru[i] = xi2
-	mockhexadeca[i] = xi4
 
-# calculate the covariance
-mockmono  = np.array(mockmono).T
-mockquadru= np.array(mockquadru).T
-mockhexadeca= np.array(mockhexadeca).T
-covmono   = np.cov(mockmono)
-covquadru = np.cov(mockquadru)
-covhexadeca = np.cov(mockhexadeca)
+		# calculate the 2pcf(mu,s)
+		dd = dd.reshape(nfile,nbins,nmu)
+		dr = dr.reshape(nfile,nbins,nmu)
+		rr = rr.reshape(nfile,nbins,nmu)
+		mono = (dd-2*dr+rr)/rr
+		quad = mono * 2.5 * (3 * mu**2 - 1)
+		hexa = mono * 1.125 * (35 * mu**4 - 30 * mu**2 + 3)
+		# integral 2pcf with mu to have 2pcf(s)
+		mono_tmp = np.trapz(mono, dx=1./nmu, axis=-1)
+		quad_tmp = np.trapz(quad, dx=1./nmu, axis=-1)
+		hexa_tmp = np.trapz(hexa, dx=1./nmu, axis=-1) 
+		#mockmono[i] = xi0
+		#mockquadru[i] = xi2
+		#mockhexadeca[i] = xi4
+		"""
+		# calculate 2pcf multipoles and to form a nbins*nfiles matrix
+		for i in range(len(ddpath)):
+			# read pair-counts files:
+			dd = ascii.read(ddpath[i],format='no_header')
+			dr = ascii.read(ddpath[i][:-2]+'dr',format='no_header')
+			rr = ascii.read(ddpath[i][:-2]+'rr',format='no_header')
+			# calculate the 2pcf(mu,s)
+			mu = (dd['col1']+dd['col2'])/2
+			s = ((dd['col3']+dd['col4'])/2).reshape(nbins,nmu)[:,0]
+			mono = (dd['col6']-2*dr['col6']+rr['col6'])/rr['col6']
+			quad = mono * 2.5 * (3 * mu**2 - 1)
+			hexa = mono * 1.125 * (35 * mu**4 - 30 * mu**2 + 3)
+			# integral 2pcf with mu to have 2pcf(s)
+			xi0 = np.trapz(mono.reshape(nbins,nmu), dx=1./nmu, axis=1)
+			xi2 = np.trapz(quad.reshape(nbins,nmu), dx=1./nmu, axis=1)
+			xi4 = np.trapz(hexa.reshape(nbins,nmu), dx=1./nmu, axis=1) 
+			mockmono[i] = xi0
+			mockquadru[i] = xi2
+			mockhexadeca[i] = xi4
+			if (i%100==0)|(i==999):
+				print('LRG_'+GC+'_mock ',np.ceil(i/10),'% 2pcf completed.')
+		"""  
+		# calculate the covariance
+		mockmono  = mono_tmp.T
+		mockquad  = np.vstack((mockmono,quad_tmp.T))
+		mockhexa  = np.vstack((mockquad,hexa_tmp.T))
+		covmono   = np.cov(mockmono)
+		covquad = np.cov(mockquad)
+		covhexa = np.cov(mockhexa)
+		print('shape of the covariance matrices for [mono],[mono,quadru] and [mono,quad,hexa]')
+		print(covmono.shape,covquad.shape,covhexa.shape)
 
-# save data as binary table
-# name of the mock 2pcf and covariance matrix file(function return)
-mockfits = home+'2PCF/mockcov_'+GC+'.fits.gz'
-if os.path.exists(mockfits):
-	os.remove(mockfits) 
-covfmt = str(nbins)+'D'
-mockfmt= str(len(ddpath))+'D'
-cols = []
-cols.append(fits.Column(name='mono',format=mockfmt, array=mockmono))
-cols.append(fits.Column(name='quadru',format=mockfmt, array=mockquadru))
-cols.append(fits.Column(name='hexa',format=mockfmt, array=mockhexadeca))
-cols.append(fits.Column(name='covmono',format=covfmt, array=covmono))
-cols.append(fits.Column(name='covquadru',format=covfmt, array=covquadru))
-cols.append(fits.Column(name='covhexa',format=covfmt, array=covhexadeca))
-hdulist = fits.BinTableHDU.from_columns(cols)
-hdulist.writeto(mockfits, overwrite=True)
+		# save data as binary table
+		# name of the mock 2pcf and covariance matrix file(function return)
+		for name,mockarr,cova in zip(['mono','quad','hexa'],[mockmono,mockquad,mockhexa],[covmono,covquad,covhexa]):
+			cols = []
+			cols.append(fits.Column(name=name,format=str(len(ddpath))+'D',array=mockarr))
+			cols.append(fits.Column(name='cov'+name,format=str(len(cova))+'D',array=cova))
+			
+			hdulist = fits.BinTableHDU.from_columns(cols)
+			hdulist.header.update(rmin=0,rmax=50,sbins=nbins,nmu=nmu)
+			hdulist.writeto(mockfits[:55]+name+mockfits[-8:], overwrite=True)
 
-time_end=time.time()
-print('Covariance matrix calculation costs',time_end-time_start,'s')
+		time_end=time.time()
+		print('Covariance matrix calculation costs',time_end-time_start,'s')
 
