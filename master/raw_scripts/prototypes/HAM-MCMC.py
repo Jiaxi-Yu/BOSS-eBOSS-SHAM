ce289288f6d7db82c915d60042a4e23ec3c4b265
jiaxi <yujx_jessy@outlook.com>
2020-09-10 22:08:10 +0200

    out-of-date and erroneously use trapz instead of sum

---
+++ master/raw_scripts/prototypes/HAM-MCMC.py
@@ -0,0 +1,273 @@
+import matplotlib 
+matplotlib.use('agg')
+import time
+init = time.time()
+import numpy as np
+from astropy.table import Table
+from astropy.io import fits
+from Corrfunc.theory.DDsmu import DDsmu
+import os
+from covmatrix import covmatrix
+from obs import obs
+import warnings
+import matplotlib.pyplot as plt
+from multiprocessing import Pool 
+from itertools import repeat
+import glob
+from getdist import plots, MCSamples, loadMCSamples
+import getdist
+import sys
+import pymultinest
+
+# variables
+gal      = sys.argv[1]
+GC       = sys.argv[2]
+date     = sys.argv[3]
+#mode     = sys.argv[4]
+npoints  = 100#int(sys.argv[3])
+nseed    = 10
+rscale   = 'linear' # 'log'
+multipole= 'quad' # 'mono','quad','hexa'
+var      = 'Vpeak'  #'Vmax' 'Vpeak'
+boxsize  = 1000
+rmin     = 5
+rmax     = 25
+nthread  = 64
+autocorr = 1
+mu_max   = 1
+nmu      = 120
+autocorr = 1
+Om = 0.31
+home      = '/global/cscratch1/sd/jiaxi/master/'
+fileroot = 'MCMCout/'+date+'/'+gal+'_'+GC+'/multinest_'
+#if os.path.exists('MCMCout/'+date+'/HAM_'+gal+'_'+GC)==False:
+#    os.makedirs('MCMCout/'+date+'/HAM_'+gal+'_'+GC) #easily cause error
+
+# covariance matrix and the observation 2pcf path
+if gal == 'ELG':
+    LRGnum   = int(2.93e5)
+    zmin     = 0.6
+    zmax     = 1.1
+    z = 0.8594
+    precut   = 80
+    mockdir  = '/global/cscratch1/sd/zhaoc/EZmock/2PCF/ELGv7_nosys_rmu/z'+str(zmin)+'z'+str(zmax)+'/2PCF/'
+    obsname  = home+'catalog/pair_counts_s-mu_pip_eBOSS_'+gal+'_'+GC+'_v7.dat'
+    halofile = home+'catalog/UNIT_hlist_0.53780.fits.gz' 
+if gal == 'LRG':
+    LRGnum   = int(6.26e4)
+    zmin     = 0.6
+    zmax     = 1.0
+    z = 0.7018
+    precut   = 160
+    mockdir  = '/global/cscratch1/sd/zhaoc/EZmock/2PCF/LRGv7_syst/z'+str(zmin)+'z'+str(zmax)+'/2PCF/'
+    obsname  = home+'catalog/pair_counts_s-mu_pip_eBOSS+SEQUELS_'+gal+'_'+GC+'_v7_2.dat'
+    halofile = home+'catalog/UNIT_hlist_0.58760.fits.gz' 
+
+# cosmological parameters
+Ode = 1-Om
+H = 100*np.sqrt(Om*(1+z)**3+Ode)
+
+# generate separation bins
+if rscale=='linear':
+    bins  = np.arange(rmin,rmax+1,1)
+    nbins = len(bins)-1
+    binmin = rmin
+    binmax = rmax
+if rscale=='log':
+    nbins = 50
+    bins=np.logspace(np.log10(rmin),np.log10(rmax),nbins+1)
+    print('Note: the covariance matrix should also change to log scale.')
+if (rmax-rmin)/nbins!=1:
+    warnings.warn("the fitting should have 1Mpc/h bin to match the covariance matrices and observation.")
+# generate mu bins   
+s = (bins[:-1]+bins[1:])/2
+mubins = np.linspace(0,mu_max,nmu+1)
+mu = (mubins[:-1]+mubins[1:]).reshape(1,nmu)/2+np.zeros((nbins,nmu))
+
+# Analytical RR calculation
+RR_counts = 4*np.pi/3*(bins[1:]**3-bins[:-1]**3)/(boxsize**3)	
+rr=((RR_counts.reshape(nbins,1)+np.zeros((1,nmu)))/nmu)
+print('the analytical random pair counts are ready.')
+
+# create the halo catalogue and plot their 2pcf
+print('reading the halo catalogue for creating the galaxy catalogue...')
+halo = fits.open(halofile)
+# whether to conduct the precut to speed up the code
+#if mode=='precut':
+#    sel=(halo[1].data[var]>precut)
+#    halo[1].data = halo[1].data[sel]
+    
+# make sure len(data) is even
+if len(halo[1].data)%2==1:
+    data = halo[1].data[:-1]
+else:
+    data = halo[1].data
+halo.close()
+
+print('selecting only the necessary variables...')
+datac = np.zeros((len(data),4))
+for i,key in enumerate(['X','Y','Z','VZ']):
+    datac[:,i] = np.copy(data[key])
+V = np.copy(data[var]).astype('float32')
+datac = datac.astype('float32')
+half = int(len(data)/2)
+
+# generate nseed Gaussian random number arrays in a list
+print('generating uniform random number arrays...')
+uniform_randoms = [np.random.RandomState(seed=1000*x).rand(len(data)).astype('float32') for x in range(nseed)] 
+uniform_randoms1 = [np.random.RandomState(seed=1050*x+1).rand(len(data)).astype('float32') for x in range(nseed)] 
+print('the uniform random number dtype is ',uniform_randoms[0].dtype)
+
+# generate covariance matrices and observations
+covfits = home+'2PCF/obs/cov_'+gal+'_'+GC+'_'+multipole+'.fits.gz'  
+#randname = obsname[:-8]+'ran.fits'
+obs2pcf  = home+'2PCF/obs/'+gal+'_'+GC+'.dat'
+covmatrix(home,mockdir,covfits,gal,GC,zmin,zmax,Om,os.path.exists(covfits))
+obs(home,gal,GC,obsname,obs2pcf,rmin,rmax,nbins,zmin,zmax,Om,os.path.exists(obs2pcf))
+# Read the covariance matrices and observations
+hdu = fits.open(covfits) # cov([mono,quadru])
+Nmock = (hdu[1].data[multipole]).shape[1] # Nbins=np.array([Nbins,Nm])
+errbar = np.std(hdu[1].data[multipole],axis=1)
+hdu.close()
+obscf = Table.read(obs2pcf,format='ascii.no_header')[binmin:binmax]  # obs 2pcf
+print('the covariance matrix and the observation 2pcf vector are ready.')
+
+
+# HAM application
+def sham_tpcf(uniform,sigma_high,v_high):
+    datav = np.copy(V)
+    # shuffle the halo catalogue and select those have a galaxy inside
+    rand1 = np.append(sigma_high*np.sqrt(-2*np.log(uniform[:half]))*np.cos(2*np.pi*uniform[half:]),sigma_high*np.sqrt(-2*np.log(uniform[:half]))*np.sin(2*np.pi*uniform[half:])) # 2.9s
+    datav*=( 1+rand1) #0.5s
+    org3  = datac[(datav<v_high)]  # 4.89s
+    LRGscat = org3[np.argpartition(-datav[(datav<v_high)],LRGnum)[:LRGnum]] #3.06s
+    # transfer to the redshift space
+    z_redshift  = (LRGscat[:,2]+LRGscat[:,3]*(1+z)/H)
+    z_redshift %=boxsize
+    # count the galaxy pairs and normalise them
+    DD_counts = DDsmu(autocorr, nthread,bins,mu_max, nmu,LRGscat[:,0],LRGscat[:,1],z_redshift,periodic=True, verbose=True,boxsize=boxsize)
+    # calculate the 2pcf and the multipoles
+    mono = (DD_counts['npairs'].reshape(nbins,nmu)/(LRGnum**2)/rr-1)
+    quad = mono * 2.5 * (3 * mu**2 - 1)
+    hexa = mono * 1.125 * (35 * mu**4 - 30 * mu**2 + 3)
+    # use trapz to integrate over mu
+    xi0_single = np.trapz(mono, dx=1./nmu, axis=-1)
+    xi2_single = np.trapz(quad, dx=1./nmu, axis=-1)
+    xi4_single = np.trapz(hexa, dx=1./nmu, axis=-1)
+    return [xi0_single,xi2_single,xi4_single]
+
+#from functools import partial
+def chi2(sigma_high,v_high):
+# calculate mean monopole in parallel
+    with Pool(processes = nseed) as p:
+        xi0_tmp = p.starmap(sham_tpcf,zip(uniform_randoms,repeat(np.float32(sigma_high)),repeat(np.float(v_high))))
+    print('a second calculation')
+    with Pool(processes = nseed) as p:
+        xi1_tmp = p.starmap(sham_tpcf,zip(uniform_randoms1,repeat(np.float32(sigma_high)),repeat(np.float(v_high))))
+    
+     # average the result for multiple seeds
+    xi0,xi2,xi4 = (np.mean(xi0_tmp,axis=0,dtype='float32')[0]+np.mean(xi1_tmp,axis=0,dtype='float32')[0])/2,(np.mean(xi0_tmp,axis=0,dtype='float32')[1]+np.mean(xi1_tmp,axis=0,dtype='float32')[1])/2,(np.mean(xi0_tmp,axis=0,dtype='float32')[2]+np.mean(xi1_tmp,axis=0,dtype='float32')[2])/2
+
+    # identify the fitting multipoles
+    if multipole=='mono':
+        model = xi0
+        mocks = hdu[1].data[multipole][binmin:binmax,:]
+        covcut = np.cov(mocks).astype('float32')
+        OBS   = obscf['col3'].astype('float32')
+    if multipole=='quad':
+        model = np.append(xi0,xi2)
+        mocks = np.vstack((hdu[1].data[multipole][binmin:binmax,:],hdu[1].data[multipole][binmin+200:binmax+200,:]))
+        covcut  = np.cov(mocks).astype('float32')
+        OBS   = np.append(obscf['col3'],obscf['col4']).astype('float32')  
+    if multipole=='hexa':
+        model = np.append(xi0,xi2,xi4)
+        mocks = np.vstack((hdu[1].data[multipole][binmin:binmax,:],hdu[1].data[multipole][binmin+200:binmax+200,:],hdu[1].data[multipole][binmin+400:binmax+400,:]))
+        covcut  = np.cov(mocks).astype('float32')
+        OBS   = np.append(obscf['col3'],obscf['col4'],obscf['col5']).astype('float32')
+
+    # calculate the covariance, residuals and chi2
+    Nbins = len(model)
+    covR  = np.linalg.inv(covcut)*(Nmock-Nbins-2)/(Nmock-1)
+    res = OBS-model
+    return res.dot(covR.dot(res))
+
+# prior
+def prior(cube, ndim, nparams):
+    if gal=='LRG':
+        cube[0] = 0.7*cube[0]+0.3 # uniform between [0.3,1.0]
+        cube[1] = 900*cube[1]+600  # uniform between [600,1500]
+    if gal=='ELG':
+        cube[0] = 1.5*cube[0] # uniform between [0,1.5]
+        cube[1] = 600*cube[1]  # uniform between [0,600]
+
+# loglikelihood = -0.5*chi2    
+def loglike(cube, ndim, nparams):
+    sigma,vcut = cube[0],cube[1]
+    return -0.5*chi2(sigma,vcut)   
+    
+# number of dimensions our problem has
+parameters = ["sigma","Vceil"]
+npar = len(parameters)
+
+# run MultiNest & write the parameter's name
+pymultinest.run(loglike, prior, npar,n_live_points= npoints, outputfiles_basename=fileroot, resume =True, verbose = True,n_iter_before_update=5,write_output=True)
+f=open(fileroot+'.paramnames', 'w')
+for param in parameters:
+    f.write(param+'\n')
+f.close()
+
+fin = time.time()  
+print('the total {} in {} SHAM costs {:.6} s \n'.format(gal,GC,fin-init))
+
+# getdist plot
+sample = loadMCSamples(fileroot)
+plt.rcParams['text.usetex'] = False
+g = plots.get_single_plotter()
+g.settings.figure_legend_frame = False
+g.settings.alpha_filled_add=0.4
+#g.settings.title_limit_fontsize = 14
+g = plots.get_subplot_plotter()
+g.triangle_plot(sample,['sigma', 'Vceil'], filled=True)#,title_limit=1)
+g.export('posterior_{}_{}_nseed{}.pdf'.format(gal,GC,nseed*2))
+plt.close('all')
+# results
+print('Results:')
+stats = sample.getMargeStats()
+best = np.zeros(npar)
+lower = np.zeros(npar)
+upper = np.zeros(npar)
+mean = np.zeros(npar)
+sigma = np.zeros(npar)
+for i in range(npar):
+    par = stats.parWithName(parameters[i])
+    #best[i] = par.bestfit_sample
+    mean[i] = par.mean
+    sigma[i] = par.err
+    lower[i] = par.limits[0].lower
+    upper[i] = par.limits[0].upper
+    best[i] = (lower[i] + upper[i]) * 0.5
+    print('getdist {0:s}: [{1:.6f}, {2:.6f}]'.format( \
+        parameters[i],  lower[i], upper[i]))
+
+
+a = pymultinest.Analyzer(npar, outputfiles_basename = fileroot)
+stats = a.get_stats()    
+    
+file = 'HAM-MCMC_'+gal+'_'+GC+'_report.txt'
+f = open(file,'a')
+f.write('{} {} multinest: \n'.format(gal,GC))
+f.write('(-2)* max loglike: {} \n'.format(-2*a.get_best_fit()['log_likelihood']))
+f.write('max-loglike params: {}\n'.format(a.get_best_fit()['parameters']))
+
+f.write('getdist 1-sigma errors: sigma [{:.6},{:.6}], vcut [{:.6},{:.6}] km/s \n'.format(lower[0],upper[0],lower[1],upper[1]))
+f.write('another way around: sigma {0:.6}+{1:.6}{2:.6}, vcut {3:.6}+{4:.6}{5:.6}km/s \n'.format(a.get_best_fit()['parameters'][0],upper[0]-a.get_best_fit()['parameters'][0],lower[0]-a.get_best_fit()['parameters'][0],a.get_best_fit()['parameters'][1],upper[1]-a.get_best_fit()['parameters'][1],lower[1]-a.get_best_fit()['parameters'][1]))
+
+    
+for j in range(npar):
+    lower[j], upper[j] = stats['marginals'][j]['1sigma']
+    print('getdist {0:s}: [{1:.6f} {2:.6f}]'.format(parameters[j],  upper[j], lower[j]))
+
+f.write('multinest analyser 1-sigma errors: sigma [{0:.6},{1:.6}], vcut [{2:.6},{3:.6}] km/s \n'.format(lower[0],upper[0],lower[1],upper[1]))
+f.write('another way around: sigma {0:.6}+{1:.6}{2:.6}, vcut {3:.6}+{4:.6}{5:.6}km/s \n'.format(a.get_best_fit()['parameters'][0],upper[0]-a.get_best_fit()['parameters'][0],lower[0]-a.get_best_fit()['parameters'][0],a.get_best_fit()['parameters'][1],upper[1]-a.get_best_fit()['parameters'][1],lower[1]-a.get_best_fit()['parameters'][1]))    
+
+f.close()
