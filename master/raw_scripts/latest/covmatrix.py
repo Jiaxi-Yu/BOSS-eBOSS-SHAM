4fc8a8296cc5dd5d5f34b7ab82bd3d0c796eae50
jiaxi <yujx_jessy@outlook.com>
2020-09-13 10:23:16 +0200

    realise data for log s bins at different z range

--- master/raw_scripts/latest/covmatrix.py
+++ master/raw_scripts/latest/covmatrix.py
@@ -2,7 +2,6 @@
 # save both the 1000 mock 2pcf and its covariance
 # and calculate the 2pcf of observations
 import time
-time_start=time.time()
 import numpy as np
 from astropy.table import Table
 from astropy.io import ascii
@@ -10,43 +9,57 @@ from astropy.io import fits
 import os
 import glob
 from NGC_SGC import read_xi
+import sys
+
 
 Om = 0.31
 home      = '/global/cscratch1/sd/jiaxi/master/'
-function  = 'mps'
-rscale = 'linear'
+function  = 'mps' #not yet
+gal  = sys.argv[1]
+rscale = sys.argv[2] #'linear' # 'log'
 
-
-for gal in ['LRG','ELG']:
+if (gal=='ELG')&(rscale == 'linear'):
+    Zrange =  [0.6,\
+               1.1]
     # EZmocks directory
-    if gal == 'ELG':
-        mockdir  = '/global/cscratch1/sd/zhaoc/EZmock/2PCF/ELGv7_nosys_rmu/z0.6z1.1/2PCF/'
-        zrange='z0.6z1.1'
-        if rscale == 'linear':
-            mockfits  = home+'catalog/nersc_mps_ELG_v7/2PCF_'+function+'_'+rscale+'_'+gal+'_mocks_'    
-        else:
-            mockfits  = home+'catalog/nersc_zbins_wp_mps_ELG/2PCF_'+function+'_'+rscale+'_'+gal+'_mocks_'  
-    if gal == 'LRG':
-        mockdir  = '/global/cscratch1/sd/zhaoc/EZmock/2PCF/LRGv7_syst/z0.6z1.0/2PCF/'
-        zrange='z0.6z1.0'            
-        if rscale == 'linear':
-            mockfits  = home+'catalog/nersc_mps_LRG_v7_2/2PCF_'+function+'_'+rscale+'_'+gal+'_mocks_'   
-        else:
-            mockfits  = home+'catalog/nersc_zbins_wp_mps_LRG/2PCF_'+function+'_'+rscale+'_'+gal+'_mocks_' 
-
     nbins=200
     nmu=120
+    znum = int(len(Zrange)/2)
+    mockdir  = '/global/cscratch1/sd/zhaoc/EZmock/2PCF/ELGv7_nosys_rmu/z0.6z1.1/2PCF/'
+    mockfits  = home+'catalog/nersc_mps_ELG_v7/2PCF_'+function+'_'+rscale+'_'+gal+'_mocks_'  
+elif (gal=='LRG')&(rscale == 'linear'):
+    Zrange =  [0.6,0.7,0.8,0.6,0.65,\
+               0.8,0.9,1.0,0.7,0.8]
+    znum = int(len(Zrange)/2)
+    mockdir  = '/global/cscratch1/sd/zhaoc/EZmock/2PCF/LRGv7_syst/z0.6z1.0/2PCF/'
+    mockfits  = home+'catalog/nersc_mps_LRG_v7_2/2PCF_'+function+'_'+rscale+'_'+gal+'_mocks_'   
+elif (rscale == 'log'):
+    nbins=len(ascii.read(home+'binfile_log.dat'))
+    nmu=120
+    if gal=='ELG':
+        Zrange =  np.array([0.6,0.7,0.8,0.9,\
+                   0.8,0.9,1.0,1.1])
+    else:
+        Zrange =  np.array([0.6,0.7,0.8,0.6,0.65,\
+                   0.8,0.9,1.0,0.7,0.8])
+    znum = int(len(Zrange)/2)
+    mockDIR  = ['{}catalog/nersc_zbins_wp_mps_{}/EZmocks/z{}z{}/2PCF/'.format(home,gal,Zrange[k],Zrange[k+znum]) for k in range(znum)]
+    mockFITS  = ['{}catalog/nersc_zbins_wp_mps_{}/{}_{}_{}_{}_{}_mocks_'.format(home,gal,function,rscale,gal,Zrange[k],Zrange[k+znum]) for k in range(znum)]
+
+for k,mockdir,mockfits in zip(range(znum),mockDIR,mockFITS): 
+    time_start=time.time()
     ddpath = glob.glob(mockdir+'*'+gal+'_NGC'+'*.dd')
     nfile = len(ddpath)
     # read all the 2pcf data
     mockmono = [x for x in range(nfile)]
     mockquad = [x for x in range(nfile)]
     mockhexa = [x for x in range(nfile)]
-    print('Ezmock reading&2PCF calculation start')
     for n in range(nfile):
-        s, mockmono[n],mockquad[n],mockhexa[n] = read_xi(mockdir+'2PCF_EZmock_eBOSS_'+gal+'_{}_v7_'+zrange+'_'+str(n+1).zfill(4)+'.{}',ds=1)
-        if (n+1)%100==0:
-            print('{} EZmock has finished {}%'.format(gal,(n+1)//10))
+        s, mockmono[n],mockquad[n],mockhexa[n] = read_xi(mockdir+'2PCF_EZmock_eBOSS_'+gal+'_{}_v7_z'+str(Zrange[k])+'z'+str(Zrange[k+znum])+'_'+str(n+1).zfill(4)+'.{}',mockdir+'2PCF_EZmock_eBOSS_'+gal+'_{}_v7_z'+str(Zrange[k])+'z'+str(Zrange[k+znum])+'.rr',ns=nbins)
+        if n==0:
+            print('Ezmock reading&2PCF calculation start')
+        elif (n+1)%100==0:
+            print('{} {} bin EZmock at {}<z<{} has finished {}%'.format(gal,rscale,Zrange[k],Zrange[k+znum],(n+1)//10))
     print('all finished')
 
     # calculate the covariance
