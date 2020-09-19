e3501fdb60ce5206f2fce038984370212f7b286a
jiaxi <yujx_jessy@outlook.com>
2020-09-19 18:48:17 +0200

    2pcf tests for SHAM gal with statistical uncertainty

--- master/raw_scripts/latest/mtsham_test.py
+++ master/raw_scripts/latest/mtsham_test.py
@@ -1,25 +1,22 @@
 import matplotlib 
 matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec 
 import time
 initial = time.time()
 import numpy as np
-from numpy import log, pi,sqrt,cos,sin,argpartition,copy,float32,int32,append,mean,cov,vstack
+from numpy import log, pi,sqrt,cos,sin,argpartition,copy,float32,int32,append,mean,cov,vstack,std
 from astropy.table import Table
 from astropy.io import fits
 from Corrfunc.theory.DDsmu import DDsmu
 import os
-import matplotlib.pyplot as plt
 from multiprocessing import Pool 
 from itertools import repeat
 import glob
-import matplotlib.gridspec as gridspec
 import sys
-
 
 gal      = 'LRG'
 GC       = 'NGC'
-#mode     = sys.argv[4]
-nseed    = 30
 rscale   = 'linear' # 'log'
 multipole= 'quad' # 'mono','quad','hexa'
 var      = 'Vpeak'  #'Vmax' 'Vpeak'
@@ -35,18 +32,11 @@ Om = 0.31
 home      = '/global/cscratch1/sd/jiaxi/master/'
 
 # covariance matrix and the observation 2pcf path
-if gal == 'ELG':
-    SHAMnum   = int(2.93e5)
-    zmin     = 0.6
-    zmax     = 1.1
-    z = 0.8594
-    halofile = home+'catalog/UNIT_hlist_0.53780.fits.gz'
 if gal == 'LRG':
     SHAMnum   = int(6.26e4)
     zmin     = 0.6
     zmax     = 1.0
     z = 0.7018
-    halofile = home+'catalog/UNIT_hlist_0.58760.fits.gz'
 
 # cosmological parameters
 Ode = 1-Om
@@ -75,45 +65,41 @@ print('reading the halo catalogue for creating the galaxy catalogue...')
 sig = sys.argv[1]
 sigV= sys.argv[2]
 Vceil = sys.argv[3]
-plot = sys.argv[4]
-sham = sys.argv[5]
+nseed    = int(sys.argv[4])
+if nseed==1:
+    extra = 1
+else:
+    extra=2
+if os.path.exists(home+'LRG_NGC-redshift_space_sigma{}_sigmaV{}_Vceil{}_seed{}-python.dat'.format(sig,sigV,Vceil,nseed*extra))==False:
+    halofile = home+'catalog/UNIT4LRG-cut200.dat'
+    print('reading UNIT catalog')
+    data = Table.read(halofile,format='ascii.no_header')
 
-if plot =='0':
-    halo = fits.open(halofile)
-    # make sure len(data) is even
-    if len(halo[1].data)%2==1:
-        data = halo[1].data[:-1]
-    else:
-        data = halo[1].data
-    halo.close()
-
-    print('selecting only the necessary variables...')
+    # make sure len(datac) is even
+    if len(data)%2==1:
+        data = data[:-1]
     datac = np.zeros((len(data),5))
-    for i,key in enumerate(['X','Y','Z','VZ',var]):
-        datac[:,i] = np.copy(data[key])
-    datac = datac.astype('float32')
+    for j in range(5):
+        datac[:,j] = np.array(data['col{}'.format(j+1)])
+
     half = int(len(data)/2)
     scathalf = int(SHAMnum/2)
 
     # generate nseed Gaussian random number arrays in a list
     print('generating uniform random number arrays...')
-    uniform_randoms = [np.random.RandomState(seed=x+1).rand(len(data)).astype('float32') for x in range(nseed)]
-    uniform_randoms1 = [np.random.RandomState(seed=x+31).rand(len(data)).astype('float32') for x in range(nseed)]
-    print('the uniform random number dtype is ',uniform_randoms[0].dtype)
+    uniform_randoms = [np.random.RandomState(seed=x+1).rand(len(datac)).astype('float32') for x in range(nseed)]
+    uniform_randoms1 = [np.random.RandomState(seed=x+1+nseed).rand(len(datac)).astype('float32') for x in range(nseed)]
 
     # HAM application
-    def sham_tpcf(uni,uni1,sigM,sigV,Mtrun):
+    def sham_tpcf(uni,uni1,,uni2,uni3,sigM,sigV,Mtrun):
         x00,x20,x40,x001,x201,x401=sham_cal(uni,sigM,sigV,Mtrun)
-        print('first finished')
         x01,x21,x41,x011,x211,x411=sham_cal(uni1,sigM,sigV,Mtrun)
-        print('second finished')
-        return  [(x00+x01)/2,(x20+x21)/2,(x40+x41)/2,(x001+x011)/2,(x201+x211)/2,(x401+x411)/2]
+        #return  [(x00+x01)/2,(x20+x21)/2,(x40+x41)/2,(x001+x011)/2,(x201+x211)/2,(x401+x411)/2]
+        return  [x00,x01,x20,x21,x40,x41,x001,x011,x201,x211,x401,x411]
 
     def sham_cal(uniform,sigma_high,sigma,v_high):
-        datav = datac[:,-1]*(1+append(sigma_high*sqrt(-2*log(uniform[:half]))*cos(2*pi*uniform[half:]),sigma_high*sqrt(-2*log(uniform[:half]))*sin(2*pi*uniform[half:]))) #0.5s
-        print('scattered')
+        datav = datac[:,-1]*(1+append(sigma_high*sqrt(-2*log(uniform[:half]))*cos(2*pi*uniform[half:]),sigma_high*sqrt(-2*log(uniform[:half]))*sin(2*pi*uniform[half:]))) 
         LRGscat = (datac[datav<v_high])[argpartition(-datav[datav<v_high],SHAMnum)[:(SHAMnum)]]
-        print('selected')
         # transfer to the redshift space
         z_redshift  = (LRGscat[:,2]+(LRGscat[:,3]+append(sigma*sqrt(-2*log(uniform[:scathalf]))*cos(2*pi*uniform[-scathalf:]),sigma*sqrt(-2*log(uniform[:scathalf]))*sin(2*pi*uniform[-scathalf:])))*(1+z)/H)
         z_redshift %=boxsize
@@ -132,47 +118,72 @@ if plot =='0':
         # use sum to integrate over mu
         return [np.sum(mono,axis=-1)/nmu,np.sum(quad,axis=-1)/nmu,np.sum(hexa,axis=-1)/nmu,np.sum(mono1,axis=-1)/nmu,np.sum(quad1,axis=-1)/nmu,np.sum(hexa1,axis=-1)/nmu]
 
-    with Pool(processes = nseed) as p:
-        xi0_tmp = p.starmap(sham_tpcf,zip(uniform_randoms,uniform_randoms1,repeat(np.float32(sig)),repeat(np.float32(sigV)),repeat(np.float32(Vceil))))
-    xi0,xi2,xi4,xi01,xi21,xi41 = mean(xi0_tmp,axis=0,dtype='float32')[0],mean(xi0_tmp,axis=0,dtype='float32')[1],mean(xi0_tmp,axis=0,dtype='float32')[2],mean(xi0_tmp,axis=0,dtype='float32')[3],mean(xi0_tmp,axis=0,dtype='float32')[4],mean(xi0_tmp,axis=0,dtype='float32')[5]
-    print('averaged')
-    if sham=='0':
-        Table([xi0,xi2,xi4]).write(home+'LRG_NGC-redshift_space-python.dat',format = 'ascii.no_header',delimiter='\t',overwrite=True)
-        Table([xi01,xi21,xi41]).write(home+'LRG_NGC-real_space-python.dat',format = 'ascii.no_header',delimiter='\t',overwrite=True)
+    if nseed==1:
+        xi0_tmp = sham_cal(uniform_randoms[0],np.float32(sig),np.float32(sigV),np.float32(Vceil))
+        xi0,xi2,xi4,xi01,xi21,xi41 = (xi0_tmp[0]+xi0_tmp[1])/2,(xi0_tmp[2]+xi0_tmp[3])/2,(xi0_tmp[4]+xi0_tmp[5])/2,(xi0_tmp[6]+xi0_tmp[7])/2,(xi0_tmp[8]+xi0_tmp[9])/2,(xi0_tmp[10]+xi0_tmp[11])/2,
     else:
-        Table([xi0,xi2,xi4]).write(home+'LRG_NGC-redshift_space_sigma{}_sigmaV{}_Vceil{}-python.dat'.format(sig,sigV,Vceil),format = 'ascii.no_header',delimiter='\t',overwrite=True)
-        Table([xi01,xi21,xi41]).write(home+'LRG_NGC-real_space_sigma{}_sigmaV{}_Vceil{}-python.dat'.format(sig,sigV,Vceil),format = 'ascii.no_header',delimiter='\t',overwrite=True)
-
-else:
-    for i,space in enumerate(['real_space','redshift_space']):
-        if sham=='0':
-            python = Table.read(home+'{}_{}-{}-python.dat'.format(gal,GC,space),format='ascii.no_header')
-            c = Table.read(home+'{}_{}-{}-c.dat'.format(gal,GC,space),format='ascii.no_header')
-        elif Vceil!='1000':
-            python = Table.read(home+'{}_{}-{}_sigma{}_sigmaV{}_Vceil{}-python.dat'.format(gal,GC,space,sig,sigV,Vceil),format='ascii.no_header')
-            c = Table.read(home+'{}_{}-{}_sigma{}_sigmaV{}_Vceil1e99-c.dat'.format(gal,GC,space,sig,sigV),format='ascii.no_header')
-        else:
-            python = Table.read(home+'{}_{}-{}_sigma{}_sigmaV{}_Vceil{}-python.dat'.format(gal,GC,space,sig,sigV,Vceil),format='ascii.no_header')
-            c = Table.read(home+'{}_{}-{}_sigma{}_sigmaV{}_Vceil{}-c.dat'.format(gal,GC,space,sig,sigV,Vceil),format='ascii.no_header')
+        with Pool(processes = nseed) as p:
+            xi0_tmp = p.starmap(sham_tpcf,zip(uniform_randoms,uniform_randoms1,repeat(np.float32(sig)),repeat(np.float32(sigV)),repeat(np.float32(Vceil))))
             
-        # plot the 2PCF multipoles
-        fig = plt.figure(figsize=(14,8))
-        spec = gridspec.GridSpec(nrows=2,ncols=2, height_ratios=[4, 1], hspace=0.3,wspace=0.4)
-        ax = np.empty((2,2), dtype=type(plt.axes))
-        for col,covbin,name,k in zip(['col1','col2'],[int(0),int(200)],['monopole','quadrupole'],range(2)):
-            values=[np.zeros(nbins),python[col]]
-            std = [np.ones(nbins),s**2*python[col]/100]
-            for j in range(2):
-                ax[j,k] = fig.add_subplot(spec[j,k])
-                ax[j,k].plot(s,s**2*(python[col]-values[j])/std[j],c='k',label='python SHAM')
-                ax[j,k].plot(s,s**2*(c[col][binmin:]-values[j])/std[j],c='r',label='C SHAM')
-                plt.xlabel('s (Mpc $h^{-1}$)')
-                if (j==0):
-                    ax[j,k].set_ylabel('$s^2 * \\xi_{}$'.format(k*2))
-                    plt.legend(loc=0)
-                    plt.title('correlation function {}: {} in {}'.format(name,gal,GC))
-                if (j==1):
-                    ax[j,k].set_ylabel('$\Delta\\xi_{}$(%)'.format(k*2))
+            xi0,xi2,xi4,xi01,xi21,xi41 = \
+            mean(xi0_tmp,axis=0,dtype='float32')[0],\
+            mean(xi_tmp,axis=0,dtype='float32')[1],\
+            mean(xi_tmp,axis=0,dtype='float32')[2],\
+            mean(xi0_tmp,axis=0,dtype='float32')[3],\
+            mean(xi0_tmp,axis=0,dtype='float32')[4],\
+            mean(xi0_tmp,axis=0,dtype='float32')[5]
+            
+            xi0,xi2,xi4,xi01,xi21,xi41 = \
+            mean([xi0_tmp[k][j] for k in range(nseed) for j in range(2)],axis=0,dtype='float32'),\
+            mean([xi0_tmp[k][j+2] for k in range(nseed) for j in range(2)],axis=0,dtype='float32'),\
+            mean([xi0_tmp[k][j+4] for k in range(nseed) for j in range(2)],axis=0,dtype='float32'),\
+            mean([xi0_tmp[k][j+6] for k in range(nseed) for j in range(2)],axis=0,dtype='float32'),\
+            mean([xi0_tmp[k][j+8] for k in range(nseed) for j in range(2)],axis=0,dtype='float32'),\
+            mean([xi0_tmp[k][j+10] for k in range(nseed) for j in range(2)],axis=0,dtype='float32')
+            
+            xi0std,xi2std,xi4std,xi01std,xi21std,xi41std = \
+            std([xi0_tmp[k][j] for k in range(nseed) for j in range(2)],axis=0,dtype='float32'),\
+            std([xi0_tmp[k][j+2] for k in range(nseed) for j in range(2)],axis=0,dtype='float32'),\
+            std([xi0_tmp[k][j+4] for k in range(nseed) for j in range(2)],axis=0,dtype='float32'),\
+            std([xi0_tmp[k][j+6] for k in range(nseed) for j in range(2)],axis=0,dtype='float32'),\
+            std([xi0_tmp[k][j+8] for k in range(nseed) for j in range(2)],axis=0,dtype='float32'),\
+            std([xi0_tmp[k][j+10] for k in range(nseed) for j in range(2)],axis=0,dtype='float32')
 
-        plt.savefig('{}_{}_2pcf_{}_sigma{}_sigmaV{}_Vceil{}_comparison.png'.format(gal,GC,space,sig,sigV,Vceil),bbox_tight=True)
-        plt.close()
+
+    Table([xi0,xi2,xi4,xi0std,xi2std,xi4std]).write(home+'LRG_NGC-redshift_space_sigma{}_sigmaV{}_Vceil{}_seed{}-python.dat'.format(sig,sigV,Vceil,nseed*extra),format = 'ascii.no_header',delimiter='\t',overwrite=True)
+    Table([xi01,xi21,xi41,xi01std,xi21std,xi41std]).write(home+'LRG_NGC-real_space_sigma{}_sigmaV{}_Vceil{}_seed{}-python.dat'.format(sig,sigV,Vceil,nseed*extra),format = 'ascii.no_header',delimiter='\t',overwrite=True)
+
+f = open(home+'{}_{}-sigma{}_sigmaV{}_Vceil1e99_seed{}-diff.dat'.format(gal,GC,sig,sigV,nseed*extra),'w')    
+for space in ['redshift_space','real_space']:
+    python = Table.read(home+'{}_{}-{}_sigma{}_sigmaV{}_Vceil{}_seed{}-python.dat'.format(gal,GC,space,sig,sigV,Vceil,nseed*extra),format='ascii.no_header')
+    if Vceil=='1000000':
+        c = Table.read(home+'{}_{}-{}_sigma{}_sigmaV{}_Vceil1e99_seed{}-c.dat'.format(gal,GC,space,sig,sigV,nseed*extra),format='ascii.no_header')[binmin:]
+    else:
+        c = Table.read(home+'{}_{}-{}_sigma{}_sigmaV{}_Vceil{}_seed{}-c.dat'.format(gal,GC,space,sig,sigV,Vceil,nseed*extra),format='ascii.no_header')[binmin:]
+    f.write('# mono(%)  quad(%)\n')
+    f.write('#{}:\n'.format(space))
+    for arr1,arr2 in zip(np.array((c['col3']-python['col1'])/python['col1']*100),np.array((c['col4']-python['col2'])/python['col2']*100)):
+        f.write('{} {}'.format(arr1,arr2))
+        f.write('\n')
+
+
+    fig = plt.figure(figsize=(15,8))
+    spec = gridspec.GridSpec(nrows=2,ncols=2, height_ratios=[4, 1], hspace=0.3)
+    ax = np.empty((2,2), dtype=type(plt.axes))
+    for k in range(2):
+        for j in range(2):
+            ax[j,k] = fig.add_subplot(spec[j,k])
+            plt.xlabel('s (Mpc $h^{-1}$)')
+            if (j==0):
+                ax[j,k].errorbar(s,s**2*python['col{}'.format(k+1)],s**2*python['col{}'.format(k+4)],color='k', marker='.',ms=2,ecolor='k',ls="none",label='python')
+                ax[j,k].plot(s,s**2*c['col{}'.format(k+3)],c='c',alpha=0.6,label = 'c')
+                ax[j,k].set_ylabel('$\\xi_{}(s)$'.format(k*2)) 
+                plt.legend(loc=0)
+                plt.title('2PCF: {} in {}'.format(gal,GC))
+            if (j==1):
+                ax[j,k].plot(s,(c['col{}'.format(k+3)]-python['col{}'.format(k+1)])/python['col{}'.format(k+1)]*100,marker='^',color='c')
+                ax[j,k].plot(s,np.zeros_like(s),'k--')
+                ax[j,k].set_ylabel('$\Delta\\xi_{}$(%)'.format(k*2))
+
+    plt.savefig(home+'{}_{}-{}-sigma{}_sigmaV{}_Vceil{}_seed{}-diff.png'.format(gal,GC,space,sig,sigV,Vceil,nseed*extra),bbox_tight=True)
+    plt.close()
