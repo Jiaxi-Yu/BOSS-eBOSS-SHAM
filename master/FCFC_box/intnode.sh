6e701d97dd817d46116e65afd890817a4dc07f2c
jiaxi <yujx_jessy@outlook.com>
2020-11-24 09:59:19 +0100

    no need to have a local one

--- master/FCFC_box/intnode.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-salloc -N 1 -n 32 -c 2 -q interactive -L SCRATCH -C haswell -t 04:00:00
-
--- master/FCFC_box/setup.sh
+++
@@ -1,3 +0,0 @@
-export LD_LIBRARY_PATH=/global/homes/z/zhaoc/lib/gsl-2.4/lib:$LD_LIBRARY_PATH
-export OMP_NUM_THREADS=64
-
