776f0bb41ff0cf04fce8cd6dcfa6c1e9de7085d4
jiaxi <yujx_jessy@outlook.com>
2021-01-24 21:29:26 +0100

    EZmocks vs PIP+ANG observations in different z slices

--- SHAM/raw_scripts/latest/EZmock_log_mps_check.py
+++ SHAM/raw_scripts/latest/EZmock_log_mps_check.py
@@ -2,129 +2,124 @@ import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import log, pi,sqrt,cos,sin,argpartition,copy,trapz,float32,int32,append,mean,cov,vstack
+from astropy.table import Table
 import sys
-from astropy.io import ascii
-from NGC_SGC import read_xi
 import matplotlib.gridspec as gridspec 
 import astropy.io.fits as fits
 from  glob import glob 
 
-gal = sys.argv[1]
-zmin = sys.argv[2]
-zmax = sys.argv[3]
-rmin = int(sys.argv[4])
-rmax = int(sys.argv[5])
+gal      = sys.argv[1]
+GC       = sys.argv[2]
+rscale   = sys.argv[3] #'linear' # 'log'
+function = 'mps'
+zmin     = sys.argv[4]
+zmax     = sys.argv[5]
+rmin     = 1
+rmax     = 40
+multipole= 'hexa' # 'mono','quad','hexa'
 
-home = '/global/cscratch1/sd/jiaxi/master/'
-files = glob(home+'catalog/nersc_zbins_wp_mps_'+gal+'/EZmocks/z'+zmin+'z'+zmax+'/2PCF/2PCF_EZmock_eBOSS_'+gal+'_NGC_v7_z'+zmin+'z'+zmax+'_*.dd') 
+home     = '/global/cscratch1/sd/jiaxi/SHAM/'
+direc    = '/global/homes/j/jiaxi/'
 
-s =ascii.read(home+'binfile_log.dat',format = 'no_header')['col3']
+if (rscale=='linear')&(function=='mps'):
+    if gal == 'LRG':
+        SHAMnum   = int(6.26e4)
+        z = 0.7018
+        a_t = '0.58760'
+        ver = 'v7_2'
+    else:
+        SHAMnum   = int(2.93e5)
+        z = 0.8594
+        a_t = '0.53780'
+        ver = 'v7'
+       
+    # generate s bins
+    bins  = np.arange(rmin,rmax+1,1)
+    nbins = len(bins)-1
+    binmin = rmin
+    binmax = rmax
+    s = (bins[:-1]+bins[1:])/2
 
-if gal == 'LRG':
-    ver='v7_2'
-else:
-    ver='v7'
-
-obs =ascii.read(home+'catalog/nersc_zbins_wp_mps_{}/\
-mps_log_{}_NGC+SGC_eBOSS_{}_zs_{}-{}.dat'.format(gal,gal,ver,zmin,zmax),format = 'no_header')
-    
-if (gal=='ELG')&(sys.argv[6]=='1'):
-        # compare ELG obs with pair counts from Faizan
-    pairs = ascii.read(home+'catalog/PIP+ANG_pair_counts/pairs_s-mu_log_eBOSS_ELG_NGC+SGC_v7_pip_zs_{}0-{}0.dat'.format(zmin,zmax))
-    mu = (np.linspace(0,1,201)[1:]+np.linspace(0,1,201)[:-1])/2
-    mask = (pairs['col5']==0)
-    mon = np.zeros_like(pairs['col3'])
-    mon[~mask]=((pairs['col3'][~mask]-2*pairs['col4'][~mask]+pairs['col5'][~mask])/pairs['col5'][~mask])
-    mon = mon.reshape(33,200)
-    qua = mon * 2.5 * (3 * mu**2 - 1)
-    hexad = mon * 1.125 * (35 * mu**4 - 30 * mu**2 + 3)
-    ## use trapz to integrate over mu
-    #** modification made
-    obs0 = np.sum(mon,axis=1)/200.
-    obs1 = np.sum(qua,axis=1)/200.
-    obs2 = np.sum(hexad,axis=1)/200.
-    print('monopole are the same? :',max(abs((obs0-obs['col4']))))
-    print('quadrupole diff:',max(abs((obs1-obs['col5']))))
-    print('hexadecapole diff:',max(abs((obs2-obs['col6']))))
-
-obs = obs[(obs['col3']>=min(s))&(obs['col3']<=max(s))]  
-
-# linear EZmock results
-#hdu = fits.open('/global/cscratch1/sd/jiaxi/master/catalog/nersc_mps_{}_{}/2PCF_mps_linear_{}_mocks_hexa.fits.gz'.format(gal,ver,gal))
-#cov = hdu[1].data['NGC+SGCmocks'][:,int(num)-1]
-#slin = (np.arange(0,200,1)+ np.arange(1,201,1))/2
-
-
-fig = plt.figure(figsize=(21,8))
-spec = gridspec.GridSpec(nrows=2,ncols=3, height_ratios=[4, 1], hspace=0.3,wspace=0.4)
-ax = np.empty((2,3), dtype=type(plt.axes))
-if len(files)<20:
-    for l in range(len(files)):
-        num = ('{}'.format(l+1)).zfill(4)
-        sfalse,xi0,xi2,xi4 = read_xi(home+'catalog/nersc_zbins_wp_mps_'+gal+'/EZmocks/z'+zmin+'z'+zmax+'/2PCF/2PCF_EZmock_eBOSS_'+gal+'_{}_v7_z'+zmin+'z'+zmax+'_'+num+'.{}',home+'catalog/nersc_zbins_wp_mps_'+gal+'/EZmocks/z'+zmin+'z'+zmax+'/2PCF/2PCF_EZmock_eBOSS_'+gal+'_{}_v7_z'+zmin+'z'+zmax+'.rr',ns=11)
-        for col,data,name,k in zip(['col4','col5','col6'],[xi0[2],xi2[2],xi4[2]],['mono','quad','hexa'],range(3)):
-            j=0
-            ax[j,k] = fig.add_subplot(spec[j,k])
-            #ax[j,k].plot(slin,slin**2*cov[200*k:200*(k+1)],'k--',alpha=0.6,label='mock linear')
-            if l==0:
-                if gal=='LRG':
-                    ax[j,k].plot(s,obs[col],'k--',alpha=0.6,label='obs')
-                else:
-                    ax[j,k].plot(s,s**2*obs[col],'k--',alpha=0.6,label='obs')
-
-            ax[j,k].plot(s,s**2*data,c='r',alpha=0.6,label='EZmock_{}'.format(num))
-            plt.xlabel('s (Mpc $h^{-1}$)')
-            ax[j,k].set_ylabel('$s^2*\\xi_{}$'.format(k*2))
-            plt.legend(loc=0)
-            plt.xlim(rmin,rmax)
-            plt.title('correlation function {}: {}'.format(name,gal))
-            j=1
-            ax[j,k] = fig.add_subplot(spec[j,k])
-            if l==0:
-                ax[j,k].plot(s,np.ones_like(s),'k--',alpha=0.6)
-            if gal=='LRG':
-                ax[j,k].plot(s,(s**2*data/obs[col]-1)*100,c='r',alpha=0.6)
-            else:
-                ax[j,k].plot(s,(data/obs[col]-1)*100,c='r',alpha=0.6)
-
-            plt.xlabel('s (Mpc $h^{-1}$)')
-            ax[j,k].set_ylabel('$\Delta\\xi_{}$(%)'.format(k*2))
-            plt.xlim(rmin,rmax)
-            plt.title('correlation function {}: {}'.format(name,gal))
-else:
-    hdu = fits.open('{}catalog/nersc_zbins_wp_mps_{}/{}_{}_{}_z{}z{}_mocks_{}.fits.gz'.format(home,gal,'mps','log',gal,zmin,zmax,'hexa'))
-    mocks = hdu[1].data['NGC+SGCmocks']
-    Ns = int(mocks.shape[0]/3)
-    median = np.nanmedian(mocks,axis=-1)
-    disp = np.std(mocks,axis=-1)
+    # covariance matrices and observations
+    obs2pcf = '{}catalog/nersc_mps_{}_{}/{}_{}_{}_{}.dat'.format(direc,gal,ver,function,rscale,gal,GC)
+    covfits  = '{}catalog/nersc_mps_{}_{}/{}_{}_{}_mocks_{}.fits.gz'.format(direc,gal,ver,function,rscale,gal,multipole)
+    # Read the covariance matrices and observations
+    hdu = fits.open(covfits) #
+    mock = hdu[1].data[GC+'mocks']
+    Nmock = mock.shape[1] 
     hdu.close()
-    for col,name,k in zip(['col4','col5','col6'],['mono','quad','hexa'],range(3)):
-        j=0
-        ax[j,k] = fig.add_subplot(spec[j,k])
-        ax[j,k].fill_between(s,s**2*((median-disp)[Ns*k:Ns*(k+1)]),s**2*((median+disp)[Ns*k:Ns*(k+1)]),label='EZmocks 1$\sigma$',color='c')
-        ax[j,k].plot(s,s**2*np.nanmedian(mocks[Ns*k:Ns*(k+1)],axis=-1),label='EZmocks median',c='r')
-        if gal=='LRG':
-            ax[j,k].plot(s,obs[col],'k',label='obs')
-        else:
-            ax[j,k].plot(s,s**2*obs[col],'k',label='obs')
-        plt.xlabel('s (Mpc $h^{-1}$)')
-        ax[j,k].set_ylabel('$s^2*\\xi_{}$'.format(k*2))
-        plt.legend(loc=0)
-        plt.xlim(rmin,rmax)
-        plt.title('correlation function {}: {}'.format(name,gal))
-        j=1
-        ax[j,k] = fig.add_subplot(spec[j,k])
-        ax[j,k].plot(s,np.ones_like(s),'k--',alpha=0.6)
-        if gal=='LRG':
-            ax[j,k].plot(s,(s**2*np.nanmedian(mocks[Ns*k:Ns*(k+1)],axis=-1)/obs[col]-1)*100,c='r',alpha=0.6)
-        else:
-            ax[j,k].plot(s,(np.nanmedian(mocks[Ns*k:Ns*(k+1)],axis=-1)/obs[col]-1)*100,c='r',alpha=0.6)
-
-        plt.xlim(rmin,rmax)
-        plt.xlabel('s (Mpc $h^{-1}$)')
-        ax[j,k].set_ylabel('$\Delta\\xi_{}$(%)'.format(k*2))
-        plt.title('correlation function {}: {}'.format(name,gal))
-
+    mocks = vstack((mock[binmin:binmax,:],mock[binmin+200:binmax+200,:],mock[binmin+400:binmax+400,:]))
+    obscf = Table.read(obs2pcf,format='ascii.no_header')[binmin:binmax]       
+    if gal == 'LRG':
+        obs   = vstack((obscf['col4'],obscf['col5'],obscf['col6']))
+    else:
+        obs   = vstack((obscf['col3'],obscf['col4'],obscf['col5']))
+        
+    print('the covariance matrix and the observation 2pcf vector are ready.')
     
-plt.savefig('2pcf_comparison_{}_z{}z{}.png'.format(gal,zmin,zmax),bbox_tight=True)
+elif (rscale=='log'):
+    # read s bins
+    binfile = Table.read(home+'binfile_log.dat',format='ascii.no_header')
+    bins  = np.unique(np.append(binfile['col1'][(binfile['col3']<rmax)&(binfile['col3']>=rmin)],binfile['col2'][(binfile['col3']<rmax)&(binfile['col3']>=rmin)]))
+    s = np.array(binfile['col3'][(binfile['col3']<rmax)&(binfile['col3']>=rmin)])
+    nbins = len(bins)-1
+    binmin = np.where(binfile['col3']>=rmin)[0][0]
+    binmax = np.where(binfile['col3']<rmax)[0][-1]+1
+    
+    if gal == 'LRG':
+        ver = 'v7_2'
+        extra = binfile['col3'][(binfile['col3']<rmax)&(binfile['col3']>=rmin)]**2
+    else:
+        ver = 'v7'
+        extra = np.ones(binmax-binmin)
+    # filenames
+    covfits = '{}catalog/nersc_zbins_wp_mps_{}/{}_{}_{}_z{}z{}_mocks_{}.fits.gz'.format(direc,gal,function,rscale,gal,zmin,zmax,multipole) 
+    obs2pcf  = '{}catalog/nersc_zbins_wp_mps_{}/{}_{}_{}_{}_eBOSS_{}_zs_{}-{}.dat'.format(direc,gal,function,rscale,gal,GC,ver,zmin,zmax)
+
+    # Read the covariance matrices 
+    hdu = fits.open(covfits) # cov([mono,quadru])
+    mocks = hdu[1].data[GC+'mocks']
+    Nmock = mocks.shape[1] 
+    hdu.close()
+    # observations
+    obscf = Table.read(obs2pcf,format='ascii.no_header')
+    obscf= obscf[(obscf['col3']<rmax)&(obscf['col3']>=rmin)]
+    # prepare OBS, covariance for chi2
+    Ns = int(mocks.shape[0]/3)
+    mocks = vstack((mocks[binmin:binmax,:],mocks[binmin+Ns:binmax+Ns,:],mocks[binmin+2*Ns:binmax+2*Ns,:]))
+    obs   = vstack((obscf['col4']/extra,obscf['col5']/extra,obscf['col6']/extra))# LRG columns are s**2*xi
+else:
+    print('wrong 2pcf function input')
+
+# plot the EZmocks vs observations
+median = np.nanmedian(mocks,axis=-1)
+disp = np.std(mocks,axis=-1)    
+# plot the 2PCF multipoles   
+fig = plt.figure(figsize=(21,8))
+spec = gridspec.GridSpec(nrows=2,ncols=3, height_ratios=[4, 1], hspace=0.2,wspace=0.3)
+ax = np.empty((2,3), dtype=type(plt.axes))
+for name,k in zip(['monopole','quadrapole','hexadecapole'],range(3)):
+    j=0
+    ax[j,k] = fig.add_subplot(spec[j,k])
+    ax[j,k].fill_between(s,s**2*((median-disp)[nbins*k:nbins*(k+1)]),s**2*((median+disp)[nbins*k:nbins*(k+1)]),label='EZmocks 1$\sigma$',color='c')
+    ax[j,k].plot(s,s**2*np.nanmedian(mocks[nbins*k:nbins*(k+1)],axis=-1),label='EZmocks median',c='r')
+    ax[j,k].plot(s,s**2*obs[k],'k',label='PIP obs')
+    plt.xlabel('s (Mpc $h^{-1}$)')
+    ax[j,k].set_ylabel('$s^2*\\xi_{}$'.format(k*2))
+    plt.legend(loc=0)
+    plt.xlim(rmin,rmax)
+    plt.title('2PCF {} for {} in {}'.format(name,gal,GC))
+    
+    # difference
+    j=1
+    ax[j,k] = fig.add_subplot(spec[j,k])
+    ax[j,k].fill_between(s,s**2*((-disp)[nbins*k:nbins*(k+1)]),s**2*((disp)[nbins*k:nbins*(k+1)]),label='EZmocks 1$\sigma$',color='c')
+    ax[j,k].plot(s,np.zeros_like(s),label='EZmocks median',c='r')
+    ax[j,k].plot(s,s**2*(obs[k]-median[nbins*k:nbins*(k+1)]),'k',label='PIP obs')
+    plt.xlabel('s (Mpc $h^{-1}$)')
+    plt.xlim(rmin,rmax)
+    ax[j,k].set_ylabel('$\Delta\\xi_{}$'.format(k*2))
+
+plt.savefig('EZmock_vs_obs_{}_{}_z{}z{}.png'.format(gal,GC,zmin,zmax),bbox_tight=True)
 plt.close()
