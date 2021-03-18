cfe3b5ce9e582835e95104b9db0820541731897f
jiaxi <yujx_jessy@outlook.com>
2021-03-18 23:25:58 +0100

    add the possibility for wp EZmocks

--- SHAM/raw_scripts/latest/covmatrix.py
+++ SHAM/raw_scripts/latest/covmatrix.py
@@ -173,6 +173,82 @@ elif (rscale=='log')&(function=='mps'):
             covR  = np.linalg.pinv(covcut)*(Nmock-Nbins-2)/(Nmock-1)
             np.savetxt(mockfits[:-23]+'covR-'+gal+'_'+GC+'-s'+str(binmin)+'_'+str(binmax)+'-z'+str(Zrange[k])+'z'+str(Zrange[k+znum])+'-quad.dat',covR)
         hdu.close()
+elif (rscale=='log')&(function=='wp'):
+    """
+    nbins=len(ascii.read(home+'binfile_log.dat'))
+    k=0
+    nmu=120
+    if gal=='ELG':
+        Zrange =  np.array([0.6,0.7,0.8,0.9,\
+                   0.8,0.9,1.0,1.1])
+    else:
+        Zrange =  np.array([0.6,0.7,0.8,0.6,0.65,\
+                   0.8,0.9,1.0,0.7,0.8])
+    znum = int(len(Zrange)/2)
+    mockDIR  = ['{}catalog/nersc_zbins_wp_mps_{}/EZmocks/z{}z{}/wp/'.format(home,gal,Zrange[k],Zrange[k+znum]) for k in range(znum)]
+    mockFITS  = ['{}catalog/nersc_zbins_wp_mps_{}/{}_{}_{}_z{}z{}_mocks_'.format(home,gal,function,rscale,gal,Zrange[k],Zrange[k+znum]) for k in range(znum)]
+
+    for k,mockdir,mockfits in zip(range(znum),mockDIR,mockFITS): 
+        time_start=time.time()
+        ddpath = glob.glob(mockdir+'*'+gal+'_NGC'+'*.dd')
+        nfile = len(ddpath)
+        # read all the 2pcf data
+        mockmono = [x for x in range(nfile)]
+        mockquad = [x for x in range(nfile)]
+        mockhexa = [x for x in range(nfile)]
+        for n in range(nfile):
+            mockmono[n],mockquad[n],mockhexa[n] = read_xi(mockdir+'2PCF_EZmock_eBOSS_'+gal+'_{}_v7_z'+str(Zrange[k])+'z'+str(Zrange[k+znum])+'_'+str(n+1).zfill(4)+'.{}',mockdir+'2PCF_EZmock_eBOSS_'+gal+'_{}_v7_z'+str(Zrange[k])+'z'+str(Zrange[k+znum])+'.rr',ns=nbins)
+            if n==0:
+                print('Ezmock reading&2PCF calculation start')
+            elif (n+1)%100==0:
+                print('{} {} bin EZmock at {}<z<{} has finished {}%'.format(gal,rscale,Zrange[k],Zrange[k+znum],(n+1)//10))
+        print('all finished')
+
+        # calculate the covariance
+        NGC  = [np.array([mockmono[k][0] for k in range(nfile)]).T,\
+                np.vstack((np.array([mockmono[k][0] for k in range(nfile)]).T,\
+                           np.array([mockquad[k][0] for k in range(nfile)]).T)),\
+                np.vstack((np.array([mockmono[k][0] for k in range(nfile)]).T,\
+                           np.array([mockquad[k][0] for k in range(nfile)]).T,\
+                           np.array([mockhexa[k][0] for k in range(nfile)]).T))]
+        SGC  = [np.array([mockmono[k][1] for k in range(nfile)]).T,\
+                np.vstack((np.array([mockmono[k][1] for k in range(nfile)]).T,\
+                           np.array([mockquad[k][1] for k in range(nfile)]).T)),\
+                np.vstack((np.array([mockmono[k][1] for k in range(nfile)]).T,\
+                           np.array([mockquad[k][1] for k in range(nfile)]).T,\
+                           np.array([mockhexa[k][1] for k in range(nfile)]).T))]
+        NGCSGC = [np.array([mockmono[k][2] for k in range(nfile)]).T,\
+                np.vstack((np.array([mockmono[k][2] for k in range(nfile)]).T,\
+                           np.array([mockquad[k][2] for k in range(nfile)]).T)),\
+                np.vstack((np.array([mockmono[k][2] for k in range(nfile)]).T,\
+                           np.array([mockquad[k][2] for k in range(nfile)]).T,\
+                           np.array([mockhexa[k][2] for k in range(nfile)]).T))]
+
+        # save data as binary table
+        # name of the mock 2pcf and covariance matrix file(function return)
+        for k,name in enumerate(['mono','quad','hexa']):
+            cols = []
+            cols.append(fits.Column(name='NGCmocks',format=str(nfile)+'D',array=NGC[k]))
+            cols.append(fits.Column(name='SGCmocks',format=str(nfile)+'D',array=SGC[k]))
+            cols.append(fits.Column(name='NGC+SGCmocks',format=str(nfile)+'D',array=NGCSGC[k]))
+
+            hdulist = fits.BinTableHDU.from_columns(cols)
+            hdulist.header.update(sbins=nbins,nmu=nmu)
+            hdulist.writeto(mockfits+name+'.fits.gz',overwrite=True)
+
+        # covR calculation for 5-25Mpc/h & quadrupole
+        hdu = fits.open(mockfits+'quad.fits.gz')
+        for GC in ['NGC','SGC','NGC+SGC']:
+            mocks = hdu[1].data[GC+'mocks'] 
+            Nmock = mocks.shape[1] 
+            Ns = int(mocks.shape[0]/2)
+            mocks = vstack((mocks[binmin:binmax,:],mocks[binmin+Ns:binmax+Ns,:]))
+            covcut  = cov(mocks).astype('float32')
+            Nbins = len(mocks)
+            covR  = np.linalg.pinv(covcut)*(Nmock-Nbins-2)/(Nmock-1)
+            np.savetxt(mockfits[:-23]+'covR-'+gal+'_'+GC+'-s'+str(binmin)+'_'+str(binmax)+'-z'+str(Zrange[k])+'z'+str(Zrange[k+znum])+'-quad.dat',covR)
+        hdu.close()
+    """        
 else:
     if gal=='LRG':
         mockdir = '{}catalog/nersc_wp_{}_{}/EZmocks/EZmock_{}_'.format(home,gal,'v7_2',gal)
