2d7a24abcc9508b92f800660fc63104275fb57f0
Jiaxi <jiaxi@work.home>
2021-04-09 00:49:07 +0200

    BOSS data preparation: calculate neff in zbins

--- SHAM/raw_scripts/latest/neff_LRG.py
+++ SHAM/raw_scripts/latest/neff_LRG.py
@@ -5,9 +5,6 @@ import sys
 
 c = 299792.458
 Om = 0.31
-zmin = np.float32(sys.argv[1])
-zmax = np.float32(sys.argv[2])
-
 
 def inv_efunc(z):
   return 1 / np.sqrt(1 - Om + Om * (1+z)**3)
@@ -48,12 +45,29 @@ def neff(z, nz, zmin=0, zmax=1):
   neff2 = simps(nz**2 * chi**2, x=chi) / simps(chi**2, x=chi)
   return np.sqrt(neff2)
 
+if sys.argv[1] == 'LOWZ':
+  fileroot = '/home/jiaxi/Desktop/BOSS_clustering/nbar_DR12v5_LOWZ_{}_om0p31_Pfkp10000.dat'
+  zmins = [0.15,0.2, 0.33,0.2]
+  zmaxs = [0.2, 0.33,0.43,0.43]
+  z, nz,nzN,nzS = combineNS(fileroot,sample = ['North','South'])
 
-z, nz,nzN,nzS = combineNS('/media/jiaxi/disk/Master/obs/nbar_eBOSS_LRG_{}GC_v7_2.dat')
-n_eff = neff(z, nz, zmin=zmin, zmax=zmax)
-print('NGC+SGC:',n_eff)
-n_eff = neff(z, nzN, zmin=zmin, zmax=zmax)
-print('NGC:',n_eff)
-n_eff = neff(z, nzS, zmin=zmin, zmax=zmax)
-print('SGC:',n_eff)
+elif sys.argv[1] == 'CMASS': 
+  fileroot = '/home/jiaxi/Desktop/BOSS_clustering/nbar_DR12v5_CMASS_{}_om0p31_Pfkp10000.dat'
+  zmins = [0.43,0.51,0.57,0.43]
+  zmaxs = [0.51,0.57,0.70,0.70]
+  z, nz,nzN,nzS = combineNS(fileroot,sample = ['North','South'])
+elif sys.argv[1] == 'eBOSS':
+  fileroot = '/media/jiaxi/disk/Master/obs/nbar_eBOSS_LRG_{}GC_v7_2.dat'
+  zmins = [0.6,0.6,0.65,0.7,0.8]
+  zmaxs = [0.7,0.8,0.8, 0.9,1.0]
+  z, nz,nzN,nzS = combineNS(fileroot)
+
+
+for zmin,zmax in zip(zmins,zmaxs):
+  n_eff = neff(z, nz, zmin=zmin, zmax=zmax)
+  print('z{}z{} NGC+SGC:{}e-4\n'.format(zmin,zmax,n_eff*1e4))
+  n_eff = neff(z, nzN, zmin=zmin, zmax=zmax)
+  print('z{}z{} NGC:{}e-4\n'.format(zmin,zmax,n_eff*1e4))
+  n_eff = neff(z, nzS, zmin=zmin, zmax=zmax)
+  print('z{}z{} SGC:{}e-4\n'.format(zmin,zmax,n_eff*1e4))
 
