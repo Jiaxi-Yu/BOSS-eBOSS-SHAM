73fb390a28be56568aab0a0fa314adc410bdf969
Jiaxi <jiaxi@work.home>
2021-04-09 00:48:39 +0200

    BOSS data preparation: clustering data to ascii

--- SHAM/raw_scripts/latest/fits2ascii.py
+++ SHAM/raw_scripts/latest/fits2ascii.py
@@ -1,21 +1,46 @@
+#!/usr/bin/env python3
 from astropy.io import ascii
 from astropy.table import Table
 import numpy as np
 from astropy.io import fits
+import sys
 
-path = '/media/jiaxi/disk/Master/OneDrive/master_thesis/obs/'
-for name in ['NGC_v7_2.dat','SGC_v7_2.dat','NGC_v7_2.ran','SGC_v7_2.ran']:
-    hdu = fits.open(path+'eBOSS_LRG_clustering_'+name+'.fits')
-    data=hdu[1].data
-    ra = data['RA']
-    dec= data['DEC']
-    z  = data['Z']
-    weight_nocoll = data['WEIGHT_SYSTOT']*data['WEIGHT_NOZ']*data['WEIGHT_FKP']
-    weight_tot    = data['WEIGHT_SYSTOT']*data['WEIGHT_NOZ']*data['WEIGHT_FKP']*data['WEIGHT_CP']
-    
-    output = Table([ra,dec,z,weight_nocoll,weight_tot], names=('RA','DEC','Z','WEIGHT_NC','WEIGHT_TOT'))
-    if name[-3:]=='dat':
-        ascii.write(output,path+name, delimiter='\t')
-    else:
-        ascii.write(output,path+name+'.dat', delimiter='\t')
-    
+if sys.argv[1] == 'LOWZ':
+    titles = ['galaxy','random0']
+    names = ['LOWZ_North','LOWZ_South']
+    path = '/home/astro/jiayu/Desktop/SHAM/catalog/BOSS_clustering/'
+elif sys.argv[1] == 'CMASS': 
+    titles = ['galaxy','random0']
+    names = ['CMASS_North','CMASS_South']
+    path = '/home/astro/jiayu/Desktop/SHAM/catalog/BOSS_clustering/'
+elif sys.argv[1] == 'eBOSS':
+    titles = ['eBOSS_LRG_clustering_']
+    names = ['NGC_v7_2.dat','SGC_v7_2.dat','NGC_v7_2.ran','SGC_v7_2.ran']
+    path = '/media/jiaxi/disk/Master/OneDrive/master_thesis/obs/'
+
+for title in titles:
+    for i,name in enumerate(names):
+        if sys.argv[1] == 'eBOSS':
+            hdu = fits.open(path+title+name+'.fits')
+        else:
+            hdu = fits.open(path+title+'_DR12v5_'+name+'.fits.gz')
+        data=hdu[1].data
+        ra = data['RA']
+        dec= data['DEC']
+        z  = data['Z']
+        if sys.argv[1] == 'eBOSS':
+            weight_tot    = data['WEIGHT_SYSTOT']*data['WEIGHT_NOZ']*data['WEIGHT_CP']*data['WEIGHT_FKP']
+        elif (sys.argv[1] == 'CMASS')&(title=='galaxy'): 
+            weight_tot    = data['WEIGHT_SYSTOT']*(data['WEIGHT_NOZ']+data['WEIGHT_CP']-1)*data['WEIGHT_FKP']
+        else:   
+            weight_tot    = data['WEIGHT_FKP']
+
+        output = Table([ra,dec,z,weight_tot], names=('RA','DEC','Z','WEIGHT_TOT'))
+        if sys.argv[1] == 'eBOSS': 
+            if name[-3:]=='dat':
+                ascii.write(output,path+name, delimiter='\t')
+            else:
+                ascii.write(output,path+name+'.dat', delimiter='\t')
+        else:
+            ascii.write(output,path+title+'_DR12v5_'+name+'.dat', delimiter='\t')
+        
