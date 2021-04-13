c8a548a9fefaaebc090ea282d32b79a2854356d6
Jiaxi <jiaxi@work.home>
2021-04-13 16:39:37 +0200

    CMASSLOWZTOT added

--- SHAM/raw_scripts/latest/fits2ascii.py
+++ SHAM/raw_scripts/latest/fits2ascii.py
@@ -13,6 +13,10 @@ elif sys.argv[1] == 'CMASS':
     titles = ['galaxy','random0']
     names = ['CMASS_North','CMASS_South']
     path = '/home/astro/jiayu/Desktop/SHAM/catalog/BOSS_clustering/'
+elif sys.argv[1] == 'CMASSLOWZ': 
+    titles = ['random_20x']#['galaxy','random_20x']
+    names = ['CMASSLOWZTOT_North','CMASSLOWZTOT_South']
+    path = '/global/cscratch1/sd/jiaxi/SHAM/catalog/BOSS_data/'
 elif sys.argv[1] == 'eBOSS':
     titles = ['eBOSS_LRG_clustering_']
     names = ['NGC_v7_2.dat','SGC_v7_2.dat','NGC_v7_2.ran','SGC_v7_2.ran']
@@ -30,12 +34,13 @@ for title in titles:
         z  = data['Z']
         if sys.argv[1] == 'eBOSS':
             weight_tot    = data['WEIGHT_SYSTOT']*data['WEIGHT_NOZ']*data['WEIGHT_CP']*data['WEIGHT_FKP']
-        elif (sys.argv[1] == 'CMASS')&(title=='galaxy'): 
+        elif ((sys.argv[1] == 'CMASS')&(title=='galaxy'))|((sys.argv[1] == 'CMASSLOWZTOT')&(title=='galaxy')): 
             weight_tot    = data['WEIGHT_SYSTOT']*(data['WEIGHT_NOZ']+data['WEIGHT_CP']-1)*data['WEIGHT_FKP']
         else:   
             weight_tot    = data['WEIGHT_FKP']
 
         output = Table([ra,dec,z,weight_tot], names=('RA','DEC','Z','WEIGHT_TOT'))
+        print(name+' finish')
         if sys.argv[1] == 'eBOSS': 
             if name[-3:]=='dat':
                 ascii.write(output,path+name, delimiter='\t')
