NGC+SGC for FCFC par counts(wrong normalisation)

SHAM/raw_scripts/latest/NGC_SGC.py
@@ -1,4 +1,5 @@
 import numpy as np
+# for EZmocks
 def read_cnt(ifile, ds=1, isdd=False, ns=200, nmu=120):
     d = np.loadtxt(ifile, unpack=True)
     if len(d[0]) != ns * nmu:
@@ -22,7 +23,6 @@ def read_cnt(ifile, ds=1, isdd=False, ns=200, nmu=120):
         return s, mu, cnt, ndata
 
     return None, None, cnt, None
-
 def read_xi(ifmt, rfmt=None, ds=1, ns=200, nmu=120):
     caps = ['NGC', 'SGC']
     dd = [None] * 3
@@ -61,3 +61,52 @@ def read_xi(ifmt, rfmt=None, ds=1, ns=200, nmu=120):
 
     return s, xi0, xi2, xi4
  
+# FCFC paircounts
+def comb(ifmt,rfmt,ds=1, ns=100, nmu=120):
+    caps = ['NGC', 'SGC']
+    dd = [None] * 3
+    dr = [None] * 3
+    rr = [None] * 3
+    xi0 = [None] * 3
+    xi2 = [None] * 3
+    xi4 = [None] * 3
+    num = [None] * 2
+
+    for i,cap in enumerate(caps):
+        # dd
+        ifile = ifmt.format(cap,'dd')
+        Smin,Smax,MUmin,MUmax,dd[i] = np.loadtxt(ifile,unpack=True)
+        # read the normalisation 
+        COMMENT_CHAR = '#'
+        columns = []
+        with open(ifile, 'r') as td:
+            for line in td:
+                if line[0] == COMMENT_CHAR:
+                    info = re.split(' +', line)
+                    columns.append(info)
+        num[i] = np.sqrt(np.float64(columns[2][-1][:-1]))
+        # dr,rr
+        ifile = ifmt.format(cap,'dr')
+        Smin,Smax,MUmin,MUmax,dr[i] = np.loadtxt(ifile,unpack=True)        
+        ifile = rfmt.format(cap)
+        Smin,Smax,MUmin,MUmax,rr[i] = np.loadtxt(ifile,unpack=True)
+    mu = (MUmin+MUmax)/2
+    # NGC+SGC
+    nfac = num[1] / num[0]
+    dd[2] = (dd[0] + dd[1] * nfac**2) / (1 + nfac)**2
+    dr[2] = (dr[0] + dr[1] * nfac**2) / (1 + nfac)**2
+    rr[2] = (rr[0] + rr[1] * nfac**2) / (1 + nfac)**2
+
+    for i in range(3):
+        mask = (rr[i]==0)
+        mono = np.zeros_like(rr[i])
+        mono[~mask]=(dd[i][~mask]-2*dr[i][~mask]+rr[i][~mask])/rr[i][~mask]
+        quad = mono * 2.5 * (3 * mu**2 - 1)
+        hexa = mono * 1.125 * (35 * mu**4 - 30 * mu**2 + 3)
+
+        xi0[i] = np.sum(mono.reshape(nmu,ns),axis=0)/nmu
+        xi2[i] = np.sum(quad.reshape(nmu,ns),axis=0)/nmu
+        xi4[i] = np.sum(hexa.reshape(nmu,ns),axis=0)/nmu
+
+    return [xi0, xi2, xi4]
+