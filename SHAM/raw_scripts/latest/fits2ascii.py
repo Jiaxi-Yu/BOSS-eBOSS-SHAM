43d18bc40ed2a4d5abf176f0900e344b353f31a2
Jiaxi <jiaxi@work.home>
2021-04-16 19:08:17 +0200

    LOWZ galaxy weight expression should be the same as that of CMASS

--- SHAM/raw_scripts/latest/fits2ascii.py
+++ SHAM/raw_scripts/latest/fits2ascii.py
@@ -8,13 +8,13 @@ import sys
 if sys.argv[1] == 'LOWZ':
     titles = ['galaxy','random0']
     names = ['LOWZ_North','LOWZ_South']
-    path = '/home/astro/jiayu/Desktop/SHAM/catalog/BOSS_clustering/'
+    path = '/global/cscratch1/sd/jiaxi/SHAM/catalog/BOSS_data/'
 elif sys.argv[1] == 'CMASS': 
     titles = ['galaxy','random0']
     names = ['CMASS_North','CMASS_South']
-    path = '/home/astro/jiayu/Desktop/SHAM/catalog/BOSS_clustering/'
+    path = '/global/cscratch1/sd/jiaxi/SHAM/catalog/BOSS_data/'
 elif sys.argv[1] == 'CMASSLOWZ': 
-    titles = ['random_20x']#['galaxy','random_20x']
+    titles = ['galaxy','random_20x']
     names = ['CMASSLOWZTOT_North','CMASSLOWZTOT_South']
     path = '/global/cscratch1/sd/jiaxi/SHAM/catalog/BOSS_data/'
 elif sys.argv[1] == 'eBOSS':
@@ -34,13 +34,12 @@ for title in titles:
         z  = data['Z']
         if sys.argv[1] == 'eBOSS':
             weight_tot    = data['WEIGHT_SYSTOT']*data['WEIGHT_NOZ']*data['WEIGHT_CP']*data['WEIGHT_FKP']
-        elif ((sys.argv[1] == 'CMASS')&(title=='galaxy'))|((sys.argv[1] == 'CMASSLOWZTOT')&(title=='galaxy')): 
+        elif title=='galaxy':
             weight_tot    = data['WEIGHT_SYSTOT']*(data['WEIGHT_NOZ']+data['WEIGHT_CP']-1)*data['WEIGHT_FKP']
         else:   
             weight_tot    = data['WEIGHT_FKP']
 
         output = Table([ra,dec,z,weight_tot], names=('RA','DEC','Z','WEIGHT_TOT'))
-        print(name+' finish')
         if sys.argv[1] == 'eBOSS': 
             if name[-3:]=='dat':
                 ascii.write(output,path+name, delimiter='\t')
@@ -48,4 +47,5 @@ for title in titles:
                 ascii.write(output,path+name+'.dat', delimiter='\t')
         else:
             ascii.write(output,path+title+'_DR12v5_'+name+'.dat', delimiter='\t')
-        
+        print(name+' finish')
+
