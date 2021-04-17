fac3b304eb196d0e16938a073f0ed38f34bd34e6
Jiaxi <jiaxi@work.home>
2021-04-18 01:33:45 +0200

    NGC+SGC corrected, xi and mps file saved

--- SHAM/raw_scripts/latest/NGC_SGC.py
+++ SHAM/raw_scripts/latest/NGC_SGC.py
@@ -1,4 +1,6 @@
 import numpy as np
+import re
+import os
 # for EZmocks
 def read_cnt(ifile, ds=1, isdd=False, ns=200, nmu=120):
     d = np.loadtxt(ifile, unpack=True)
@@ -63,50 +65,63 @@ def read_xi(ifmt, rfmt=None, ds=1, ns=200, nmu=120):
  
 # FCFC paircounts
 def FCFCcomb(ifmt,rfmt,ds=1, ns=100, nmu=120):
-    caps = ['NGC', 'SGC']
-    dd = [None] * 3
-    dr = [None] * 3
-    rr = [None] * 3
-    xi0 = [None] * 3
-    xi2 = [None] * 3
-    xi4 = [None] * 3
-    num = [None] * 2
+    if os.path.exists(ifmt.format('NGC+SGC','mps')):
+        xi0 = [None] * 3
+        xi2 = [None] * 3
+        xi4 = [None] * 3
+        caps = ['North','South','NGC+SGC']     
+        for j,cap in enumerate(caps):
+            smid,smin,smax,xi0[j],xi2[j],xi4[j] = np.loadtxt(ifmt.format(cap,'mps'),unpack = True)
+    else:
+        caps = ['NGC', 'SGC']
+        dd = [None] * 3
+        dr = [None] * 3
+        rr = [None] * 3
+        xi0 = [None] * 3
+        xi2 = [None] * 3
+        xi4 = [None] * 3
+        num = [None] * 2
+        norm = [None] * 2
 
-    for i,cap in enumerate(caps):
-        # dd
-        ifile = ifmt.format(cap,'dd')
-        Smin,Smax,MUmin,MUmax,dd[i] = np.loadtxt(ifile,unpack=True)
-        # read the normalisation 
-        COMMENT_CHAR = '#'
-        columns = []
-        with open(ifile, 'r') as td:
-            for line in td:
-                if line[0] == COMMENT_CHAR:
-                    info = re.split(' +', line)
-                    columns.append(info)
-        num[i] = np.float64(columns[1][-2])
-        # dr,rr
-        ifile = ifmt.format(cap,'dr')
-        Smin,Smax,MUmin,MUmax,dr[i] = np.loadtxt(ifile,unpack=True)        
-        ifile = rfmt.format(cap)
-        Smin,Smax,MUmin,MUmax,rr[i] = np.loadtxt(ifile,unpack=True)
-    mu = (MUmin+MUmax)/2
-    # NGC+SGC
-    nfac = num[1] / num[0]
-    dd[2] = (dd[0] + dd[1] * nfac**2) / (1 + nfac)**2
-    dr[2] = (dr[0] + dr[1] * nfac**2) / (1 + nfac)**2
-    rr[2] = (rr[0] + rr[1] * nfac**2) / (1 + nfac)**2
+        for i,cap in enumerate(caps):
+            # dd
+            ifile = ifmt.format(cap,'dd')
+            Smin,Smax,MUmin,MUmax,dd[i] = np.loadtxt(ifile,unpack=True)
+            # read the normalisation 
+            COMMENT_CHAR = '#'
+            columns = []
+            with open(ifile, 'r') as td:
+                for line in td:
+                    if line[0] == COMMENT_CHAR:
+                        info = re.split(' +', line)
+                        columns.append(info)
+            num[i]  = np.float64(columns[1][-2])
+            norm[i] = np.float64(columns[2][-1]) 
+            # dr,rr
+            ifile = ifmt.format(cap,'dr')
+            Smin,Smax,MUmin,MUmax,dr[i] = np.loadtxt(ifile,unpack=True)        
+            ifile = rfmt.format(cap)
+            Smin,Smax,MUmin,MUmax,rr[i] = np.loadtxt(ifile,unpack=True)
+        mu = (MUmin+MUmax)/2
+        # NGC+SGC
+        dd[2] = dd[0] * norm[0] + dd[1] * norm[1]
+        dr[2] = dr[0] * norm[0] + dr[1] * norm[1]
+        rr[2] = rr[0] * norm[0] + rr[1] * norm[1]
 
-    for i in range(3):
-        mask = (rr[i]==0)
-        mono = np.zeros_like(rr[i])
-        mono[~mask]=(dd[i][~mask]-2*dr[i][~mask]+rr[i][~mask])/rr[i][~mask]
-        quad = mono * 2.5 * (3 * mu**2 - 1)
-        hexa = mono * 1.125 * (35 * mu**4 - 30 * mu**2 + 3)
+        for i in range(3):
+            mask = (rr[i]==0)
+            mono = np.zeros_like(rr[i])
+            mono[~mask]=(dd[i][~mask]-2*dr[i][~mask]+rr[i][~mask])/rr[i][~mask]
+            quad = mono * 2.5 * (3 * mu**2 - 1)
+            hexa = mono * 1.125 * (35 * mu**4 - 30 * mu**2 + 3)
 
-        xi0[i] = np.sum(mono.reshape(nmu,ns),axis=0)/nmu
-        xi2[i] = np.sum(quad.reshape(nmu,ns),axis=0)/nmu
-        xi4[i] = np.sum(hexa.reshape(nmu,ns),axis=0)/nmu
+            xi0[i] = np.sum(mono.reshape(nmu,ns),axis=0)/nmu
+            xi2[i] = np.sum(quad.reshape(nmu,ns),axis=0)/nmu
+            xi4[i] = np.sum(hexa.reshape(nmu,ns),axis=0)/nmu
+        smin = np.unique(Smin)
+        smax = np.unique(Smax)
+        smid = (smin+smax)/2
+        np.savetxt(ifmt.format('NGC+SGC','xi'),np.array([Smin,Smax,MUmin,MUmax,dd[2],dr[2],rr[2]]).T,header='weighted galaxy: NGC {}, SGC {} \n normalisation: NGC {}, SGC {}'.format(num[0],num[1],norm[0],norm[1]))
+        np.savetxt(ifmt.format('NGC+SGC','mps'),np.array([smid,smin,smax,xi0[2],xi2[2],xi4[2]]).T,header='weighted galaxy: NGC {}, SGC {} \n normalisation: NGC {}, SGC {}'.format(num[0],num[1],norm[0],norm[1]))
 
-    return [xi0, xi2, xi4]
-
+    return xi0,xi2,xi4
