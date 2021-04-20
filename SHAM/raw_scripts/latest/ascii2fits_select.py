62bcf7e76cf8a0de42dd1bd2908a9f01056cf105
Jiaxi <jiaxi@work.home>
2021-04-20 17:10:25 +0200

    used the correct colume of Vpeak and save as an ascii file

--- SHAM/raw_scripts/latest/ascii2fits_select.py
+++ SHAM/raw_scripts/latest/ascii2fits_select.py
@@ -8,12 +8,14 @@ if len(sys.argv) != 3:
   print('Usage: {} INPUT OUTPUT'.format(sys.argv[0]), file=sys.stderr)
   sys.exit(1)
 
-data = read_csv(sys.argv[1], header=None, delim_whitespace=True, comment='#', \
-    engine='c', usecols=(1,5,10,11,12,16,17,18,19,20,21,22), \
-    names=('ID','PID','Mvir','Rvir','Rs','Vpeak','X','Y','Z','VX','VY','VZ'), \
-    dtype={'ID':np.int64, 'PID':np.int64, 'Mvir':np.float32, 'Rvir':np.float32, \
-        'Rs':np.float32,'Vpeak':np.float32, 'X':np.float32, 'Y':np.float32,\
-           'Z':np.float32,'VX':np.float32, 'VY':np.float32, 'VZ':np.float32})
 
-t = Table.from_pandas(data)
-t.write(sys.argv[2], format='fits', overwrite=True)
+  print('reading source files')
+  data = read_csv('hlist_{}.list'.format(sys.argv[1]), header=None, delim_whitespace=True, comment='#', \
+      engine='c', usecols=(1,5,17,18,19,22,62), \
+      names=('ID','PID','X','Y','Z','VZ','Vpeak'), \
+      dtype={'ID':np.int64, 'PID':np.int64,'X':np.float32,'Y':np.float32,'Z':np.float32, 'VZ':np.float32,'Vpeak':np.float32})
+
+  t = Table.from_pandas(data)
+  #t.write('UNIT_hlist_{}.fits.gz'.format(sys.argv[1]), format='fits', overwrite=True)
+  print('writing the new one')
+  t.write('UNIT_hlist_{}.dat'.format(sys.argv[2]), format='ascii.no_header', overwrite=True)
