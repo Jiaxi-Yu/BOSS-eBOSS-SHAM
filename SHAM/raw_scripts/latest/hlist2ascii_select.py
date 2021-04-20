f3eb28885dbefd593f03bc3837cb6e0dac57f82c
Jiaxi <jiaxi@work.home>
2021-04-20 17:17:13 +0200

    renamed hlist to ascii code

---
+++ SHAM/raw_scripts/latest/hlist2ascii_select.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys
+from pandas import read_csv
+import numpy as np
+from astropy.table import Table
+
+if len(sys.argv) != 2:
+  print('Usage: {} UNITa(t)'.format(sys.argv[0]), file=sys.stderr)
+  sys.exit(1)
+
+
+print('reading source files')
+data = read_csv('hlist_{}.list'.format(sys.argv[1]), header=None, delim_whitespace=True, comment='#', \
+    engine='c', usecols=(1,5,17,18,19,22,62), \
+    names=('ID','PID','X','Y','Z','VZ','Vpeak'), \
+    dtype={'ID':np.int64, 'PID':np.int64,'X':np.float32,'Y':np.float32,'Z':np.float32, 'VZ':np.float32,'Vpeak':np.float32})
+
+t = Table.from_pandas(data)
+#t.write('UNIT_hlist_{}.fits.gz'.format(sys.argv[1]), format='fits', overwrite=True)
+print('writing the new one')
+t.write('UNIT_hlist_{}.dat'.format(sys.argv[1]), format='ascii.no_header', overwrite=True)
