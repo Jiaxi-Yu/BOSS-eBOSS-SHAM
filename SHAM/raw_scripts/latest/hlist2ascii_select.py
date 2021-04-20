c459f5f55c0bdde303461bd71a56f9a447d59728
Jiaxi <jiaxi@work.home>
2021-04-20 17:22:59 +0200

    removed the ID and parent ID of the halos

--- SHAM/raw_scripts/latest/hlist2ascii_select.py
+++ SHAM/raw_scripts/latest/hlist2ascii_select.py
@@ -11,11 +11,10 @@ if len(sys.argv) != 2:
 
 print('reading source files')
 data = read_csv('hlist_{}.list'.format(sys.argv[1]), header=None, delim_whitespace=True, comment='#', \
-    engine='c', usecols=(1,5,17,18,19,22,62), \
-    names=('ID','PID','X','Y','Z','VZ','Vpeak'), \
-    dtype={'ID':np.int64, 'PID':np.int64,'X':np.float32,'Y':np.float32,'Z':np.float32, 'VZ':np.float32,'Vpeak':np.float32})
-
+    engine='c', usecols=(17,18,19,22,62), \
+    names=('X','Y','Z','VZ','Vpeak'), \
+    dtype={'X':np.float32,'Y':np.float32,'Z':np.float32, 'VZ':np.float32,'Vpeak':np.float32})
+# ID and PID are colume 1 and 5
 t = Table.from_pandas(data)
-#t.write('UNIT_hlist_{}.fits.gz'.format(sys.argv[1]), format='fits', overwrite=True)
 print('writing the new one')
 t.write('UNIT_hlist_{}.dat'.format(sys.argv[1]), format='ascii.no_header', overwrite=True)
