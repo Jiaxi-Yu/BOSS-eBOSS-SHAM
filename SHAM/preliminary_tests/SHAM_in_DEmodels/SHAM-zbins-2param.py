dc2750048144a8f2cfd7728c1c1f1e373c9a4a6f
Jiaxi <jiaxi@work.home>
2021-05-05 17:16:44 +0200

    unknown modification

--- SHAM/preliminary_tests/SHAM_in_DEmodels/SHAM-zbins-2param.py
+++ SHAM/preliminary_tests/SHAM_in_DEmodels/SHAM-zbins-2param.py
@@ -330,7 +330,7 @@ else:
                     ax[j,k].errorbar(s+0.2,s**2*(xi[:,k]-values[j]),s**2*stdSHAM[k],c='c', marker='^',ecolor='c',ls="none",alpha=0.8,label='SHAM')
                     plt.legend(loc=2)
                 else:
-                    ax[j,k].errorbar(s+0.2,s**2*(xi[:,k]-values[j]),s**2*stdSHAM[k],c='c', marker='^',ecolor='c',ls="none",alpha=0.8,label='SHAM, $\chi^2$/dof={:.4}/{}'.format(-2*a.get_best_fit()['log_likelihood'],int(2*(len(s)-3)-3)))
+                    ax[j,k].errorbar(s+0.2,s**2*(xi[:,k]-values[j]),s**2*stdSHAM[k],c='c', marker='^',ecolor='c',ls="none",alpha=0.8,label='SHAM, $\chi^2$/dof={:.4}/{}'.format(-2*a.get_best_fit()['log_likelihood'],int(2*nbins-3)))
                     plt.legend(loc=1)
                 plt.title('correlation function {}: {} in {}'.format(name,gal,GC))
             if (j==1):
