a9c62e40f604af14aaeb85247568fcf9894bcaed
Jiaxi <jiaxi@work.home>
2021-06-19 16:40:03 +0200

    BOSS matching by TARGETID and position: still missing element for LOWZ

--- SHAM/raw_scripts/latest/Vsmear_clustering-repeat_matching.py
+++ SHAM/raw_scripts/latest/Vsmear_clustering-repeat_matching.py
@@ -8,6 +8,7 @@ import os
 
 c_kms = 299792.
 min_dchi2 = 9
+fc_limit = (62/3600)**2
 
 # load the total catalogue and the repetitive samples
 scratch = '/global/cscratch1/sd/jiaxi/SHAM/catalog/'
@@ -29,22 +30,20 @@ elif gal == 'CMASS':
     clusteringN,clusteringS = scratch+'BOSS_data/galaxy_DR12v5_CMASS_North.fits.gz',scratch+'BOSS_data/galaxy_DR12v5_CMASS_South.fits.gz'
 elif gal == 'LOWZ':
     proj='BOSS'
-    zmins = [0.2, 0.33,0.2]
-    zmaxs = [0.33,0.43,0.43]
-    maxdvs = [105,140,140]
+    zmins = [0.2]#[0.2, 0.33,0.2]
+    zmaxs = [0.43]#[0.33,0.43,0.43]
+    maxdvs = [140]#[105,140,140]
     #zmin = 0.2; zmax = 0.43
     clusteringN,clusteringS = scratch+'BOSS_data/galaxy_DR12v5_LOWZ_North.fits.gz',scratch+'BOSS_data/galaxy_DR12v5_LOWZ_South.fits.gz'
 else:
     print("Wrong input")    
 
-
-hdu = fits.open('/global/cscratch1/sd/jiaxi/SHAM/catalog/spAll-zbest-v5_13_0.fits')
-complete = hdu[1].data
-hdu.close()
-targetid_tot = complete['PLATE']*1e9+complete['MJD']*1e4+complete['FIBERID']
-print('the complete sample reading finished.')
-    
+fig = plt.figure(figsize=(7*len(zmin),6))
+spec = gridspec.GridSpec(nrows=1,ncols=3,wspace=0.4)
+ax = np.empty((1,3), dtype=type(plt.axes))
+k  = 0
 for zmin,zmax,maxdv in zip(zmins,zmaxs,maxdvs):
+    # repeat samples
     repeatfile = home+'{}-{}_deltav_z{}z{}.fits.gz'.format(proj,gal,zmin,zmax)
     hdu = fits.open(repeatfile)
     reobs = hdu[1].data
@@ -52,148 +51,24 @@ for zmin,zmax,maxdv in zip(zmins,zmaxs,maxdvs):
     hdu.close()
     print('the repetitive sample sample reading finished.')
 
+    # clustering matched deltav and zerr
+    hdu = fits.open(home+'clustering_zerr1/{}_targetid_deltav_zerr_z{}z{}.fits.gz'.format(gal,zmin,zmax))
+    data = hdu[1].data
+    hdu.close()
+    LRGtot = np.zeros((len(data['zerr']),3))
+    for k,name in enumerate(data.columns.names):
+        LRGtot[:,k] = data[name]
+    
+    # binning the deltav
+    binwidth = 5
+    bins = np.arange(-max_dv, max_dv+1, binwidth)
+    dens,BINS = np.histogram(dvsel,bins=bins)
+    norm = np.sum(dens)
+    dens = dens/norm
 
-    # the matching function
-    nthreads = 64
-    def matching(index,clustering):
-        #print('thread ',index)
-        #slice the clustering catalogue in nthread parts
-        partlen = len(clustering)//nthreads
-        if index == nthreads-1:
-            TARGETID = clustering[index*partlen:]
-        else:
-            TARGETID = clustering[index*partlen:(index+1)*partlen]
+    # plot the histogram from repeat and clustering
+    ax[0,k] = fig.add_subplot(spec[0,k])
+    plt.scatter()
+    
+    k+=1
 
-        # select the clustering galaxies in the complete catalogue and the repeat catalogue
-        repz,zerr = [],[]
-        for j,target in enumerate(TARGETID):
-            # select the clustering galaxies with targetid from complete
-            if gal == 'LRG':
-                poszerr = np.where(complete['RA_REDROCK']==target)[0]
-            else:
-                poszerr = np.where(targetid_tot==target)[0]
-
-            # according to THING_ID from complete==clustering, select the repetitive measurements
-            #print(j,len(complete['THING_ID'][poszerr]))
-            #if len(complete['THING_ID'][poszerr])!=1:
-            #    print(complete['THING_ID'][poszerr],complete['RA_REDROCK'][poszerr],complete['DEC_REDROCK'][poszerr])
-                
-            if len(complete['THING_ID'][poszerr])==0:
-                zerr.append(np.nan)
-                repz.append(np.nan)  
-            else:
-                pos = np.where(reobs['thids']==np.unique(complete['THING_ID'][poszerr]))[0]
-                if len(pos)==1:
-                    # if the clustering galaxy has two measurements: zerr = zerr from repeat
-                    zerr.append(reobs['zerr'][pos][0])
-                    repz.append(reobs['delta_v'][pos][0])
-                else:
-                    # otherwise, they should be the first 
-                    zerr.append((complete['ZERR_REDROCK'][poszerr]*c_kms/(1+complete['Z_REDROCK'][poszerr]))[0])
-                    repz.append(np.nan)
-                
-
-        return np.array([TARGETID,repz,zerr]).T
-
-
-    def clusteringSEL(fileN,fileS,zmin,zmax,maxdv):
-        if os.path.exists(home+'clustering_zerr/{}_targetid_deltav_zerr_z{}z{}.fits.gz'.format(gal,zmin,zmax)):
-            hdu = fits.open(home+'clustering_zerr/{}_targetid_deltav_zerr_z{}z{}.fits.gz'.format(gal,zmin,zmax))
-            data = hdu[1].data
-            hdu.close()
-
-            LRGtot = np.zeros((len(data['zerr']),3))
-            #import pdb;pdb.set_trace()
-            for k,name in enumerate(data.columns.names):
-                LRGtot[:,k] = data[name]
-        else:
-            # read clustering data 
-            hdu = fits.open(fileN)
-            dataN = hdu[1].data
-            dataN = dataN[(dataN['Z']>zmin)&(dataN['Z']<zmax)]
-            hdu.close()
-
-            hdu = fits.open(fileS)
-            dataS = hdu[1].data
-            dataS = dataS[(dataS['Z']>zmin)&(dataS['Z']<zmax)]
-            hdu.close()
-            print('clustering data reading finished.')
-
-            # find clustering galaxies in the repetitive catalogue and the complete catalogue
-            LRG = []
-            ind = [i for i in range(nthreads)]
-            if gal == 'LRG':
-                ID = 'ra'
-                inputN = dataN['RA']
-                inputS = dataS['RA']
-            else:
-                ID = 'targetid'
-                inputN = dataN['PLATE']*1e9+dataN['MJD']*1e4+dataN['FIBERID']
-                inputS = dataS['PLATE']*1e9+dataS['MJD']*1e4+dataS['FIBERID']
-            datan = [inputN]*nthreads
-            datas = [inputS]*nthreads
-            
-            #import pdb;pdb.set_trace()
-            with Pool(processes = nthreads) as p:
-                LRG.append(p.starmap(matching,list(zip(ind,datan))))
-            print('NGC matching finished.')
-
-            with Pool(processes = nthreads) as p:
-                LRG.append(p.starmap(matching,list(zip(ind,datas))))
-            print('SGC matching finished.')
-            
-            for cap in range(2):
-                for i in range(nthreads):
-                    if (cap == 0)&(i<2):
-                        LRGtot = np.vstack((LRG[cap][0],LRG[cap][1]))
-                    else:
-                        LRGtot = np.vstack((LRGtot,LRG[cap][i]))
-            #import pdb;pdb.set_trace()
-
-            cols = []
-            formats=['K','D','D']
-            for k,colname in enumerate([ID,'delta_v','zerr']):
-                cols.append(fits.Column(name=colname,format=formats[k],array=LRGtot[:,k]))
-            hdulist = fits.BinTableHDU.from_columns(cols)
-            hdulist.writeto(home+'clustering_zerr/{}_targetid_deltav_zerr_z{}z{}.fits.gz'.format(gal,zmin,zmax),overwrite=True)
-
-        bins = np.arange(0,maxdv+1,5)
-        densdv,BINS = np.histogram(np.abs(LRGtot[~np.isnan(LRGtot[:,1]),1]),bins=bins)
-        denszerr,BINS = np.histogram(np.abs(LRGtot[~np.isnan(LRGtot[:,2]),2]),bins=bins)
-        plt.plot((BINS[1:]+BINS[:-1])/2,densdv/sum(densdv),'r',label='$\Delta$ v')
-        plt.plot((BINS[1:]+BINS[:-1])/2,denszerr/sum(denszerr),'b',label='ZERR')
-        plt.yscale('log')
-        plt.legend(loc=1)
-        plt.title('{} clustering galaxy |$\Delta$ v| (if exists) v.s. ZERR'.format(gal))
-        plt.savefig(home+'clustering_zerr/{}_clustering-deltav_zerr_z{}z{}.png'.format(gal,zmin,zmax))
-        plt.close()
-
-    # 
-    clusteringSEL(clusteringN,clusteringS,zmin,zmax,maxdv)
-    print('z{}z{} selection finished'.format(zmin,zmax))
-
-
-
-"""
-# test: 
-## parallelised
-LRG=[]
-datas = [dataS['RA'][:2000]]*nthreads
-with Pool(processes = nthreads) as p:
-    LRG = p.starmap(matching,list(zip(ind,datas))) 
-test=[]
-for i in range(nthreads):
-    if len(LRG[i])!=0:
-        for k in range(len(LRG[i])):
-            test.append(LRG[i][k])
-# loop    
-LRGloop=[]
-for j,ra in enumerate(dataS['RA'][:2000]):
-    pos = np.where(reobs['RA_REDROCK']==ra)[0]
-    if len(pos)==2:
-        LRGloop.append(pos)
-
-        # whether they equal: yes
-np.allclose(np.array(test),np.array(LRGloop))
-
-"""
