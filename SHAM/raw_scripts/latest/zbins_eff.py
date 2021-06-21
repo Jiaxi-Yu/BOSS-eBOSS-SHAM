a27be1427f9480bc309eba49fd6d41e72ab28372
Jiaxi-Yu <yujxjessy@outlook.com>
2021-06-21 22:38:01 +0200

    updated the directories and the redshift ranges for eBOSS ELG and LRG

--- SHAM/raw_scripts/latest/zbins_eff.py
+++ SHAM/raw_scripts/latest/zbins_eff.py
@@ -5,15 +5,21 @@ import sys
 zw = {}
 
 if sys.argv[1] == 'eBOSS':
-    for gal,ver in zip(['LRG','ELG'],['v7_2','v7']):
+    for gal,ver in zip(['LRG'],['v7_2']):#zip(['LRG','ELG'],['v7_2','v7']):
         f = open(gal+'zeff.txt','w')
         f.write('# zmin zmax zeff Ngal \n')
         for GC in ['NGC','SGC']:
-            filename = '/global/cscratch1/sd/jiaxi/master/catalog/eBOSS_{}_clustering_{}_{}.dat.fits'.format(gal,GC,ver)
+            filename = '/global/cscratch1/sd/jiaxi/SHAM/catalog/eBOSS_clustering_fits/eBOSS_{}_clustering_{}_{}.dat.fits'.format(gal,GC,ver)
             hdu = fits.open(filename)
             zw[gal+'_'+GC+'_z']=hdu[1].data['Z']
             zw[gal+'_'+GC+'_w']=hdu[1].data['WEIGHT_FKP']*hdu[1].data['WEIGHT_SYSTOT']*hdu[1].data['WEIGHT_CP']*hdu[1].data['WEIGHT_NOZ']
-        for zmin,zmax in zip([0.6,0.7,0.8],[0.8,0.9,1.0]):
+            if gal == 'LRG':
+                zmins = [0.6,0.6,0.65,0.7,0.8,0.6]
+                zmaxs = [0.7,0.8,0.8, 0.9,1.0,1.0]
+            else:
+                zmins = [0.6,0.7,0.8,0.9,0.6]
+                zmaxs = [0.8,0.9,1.0,1.1,1.1]
+        for zmin,zmax in zip(zmins,zmaxs):
             sel1 = (zw[gal+'_NGC_z']>zmin)&(zw[gal+'_NGC_z']<=zmax)
             sel2 = (zw[gal+'_SGC_z']>zmin)&(zw[gal+'_SGC_z']<=zmax)
 
