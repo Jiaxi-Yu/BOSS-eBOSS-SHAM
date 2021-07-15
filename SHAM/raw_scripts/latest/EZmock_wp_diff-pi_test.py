e38e05caa5cebc23d6b357d38be44d7c9ae5db7d
Jiaxi-Yu <yujxjessy@outlook.com>
2021-07-15 16:16:54 +0200

    wp pi can have both upper and lower limit

--- SHAM/raw_scripts/latest/EZmock_wp_diff-pi_test.py
+++ SHAM/raw_scripts/latest/EZmock_wp_diff-pi_test.py
@@ -39,25 +39,30 @@ elif function == 'wp':
     nmu    = 80
     func   = 'wp'
     pimaxs  = [20,40,60,70,80]
-    pimin = 0 #0
+    pimin = 5 #0
     swp = np.loadtxt(home+'binfile_log.dat',usecols=2)
     s = swp[(swp>binmin)&(swp<binmax+1)]
 
 
 #######################
 # plot mocks
-for mocktype in ['sys']:
+for mocktype in ['sys','nosys']:
     for mockweight in ['_FKP','_FKPCP','_FKPSYSTOT','_FKPNOZ']:#['_FKP','_FKPCP','_FKPNOZ','_FKPSYSTOT']:
         if mocktype == 'nosys':
             mockweight = '_FKP'
-        if mockweight == '_FKP':
+            mockstart = 501
+        elif mockweight == '_FKP':
             mockstart = 1
         else:
             mockstart = 501
         mockDIR  = ['{}EZmocks_{}/z{}z{}/clustering{}/'.format(home,mocktype,Zrange[k],Zrange[k+znum],mockweight) for k in range(znum)]
         mockFITS  = ['{}{}/EZmocks_{}_{}_{}_z{}z{}_'.format(datapath,mocktype+mockweight,mocktype,function,rscale,Zrange[k],Zrange[k+znum]) for k in range(znum)]
         flagmps = mockFITS[-1]+'quad.fits.gz'
-        flagwp = mockFITS[-1]+'wp_pi80.fits.gz'
+        if pimin == 0:
+            wptail = 'wp_pi80.fits.gz'
+        else:
+            wptail = 'wp_pi{}-80.fits.gz'.format(pimin)
+        flagwp = mockFITS[-1]+wptail
         for k,mockdir,mockfits in zip(range(znum),mockDIR,mockFITS): 
             start=time.time()
             ddpath = glob.glob(mockdir+'*_SGC'+'*.dd')
@@ -118,46 +123,14 @@ for mocktype in ['sys']:
                     hdulist = fits.BinTableHDU.from_columns(cols)
                     hdulist.header.update(sbins=nbins,nmu=nmu)
                     hdulist.writeto(mockfits+name+'.fits.gz',overwrite=True)
-            elif (function == 'wp')&(not os.path.exists(flagwp)):
-                if mocktype == 'sys':
-                    pairroot = [mockdir+'EZmock_eBOSS_{}_'+ver+'_z'+str(Zrange[k])+'z'+str(Zrange[k+znum])+'_'+str(n+mockstart).zfill(4)+'.{}.wp' for n in range(nfile)]
-                    comb_part = partial(FCFCcomb,ns=nbins,nmu=nmu,islog=True,ismps=False)
-                else:
-                    pairroot = [mockdir+'EZmock_eBOSS_{}_'+ver+'_z'+str(Zrange[k])+'z'+str(Zrange[k+znum])+'_'+str(n+mockstart).zfill(4)+'.{}.wp' for n in range(nfile)]
-                    rrroot = mockdir+'EZmock_eBOSS_{}_'+ver+'_z'+str(Zrange[k])+'z'+str(Zrange[k+znum])+'.rr.wp'
-                    comb_part = partial(FCFCcomb,rfmt = rrroot,ns=nbins,nmu=nmu,islog=True,ismps=False)
-
-                # NGC+SGC for wp
-                pool = Pool()     
-                for n, temp_array in enumerate(pool.imap(comb_part,pairroot)):
-                    mockmono[n] = temp_array
-                    if n==0:
-                        print('mock reading&2PCF calculation start')
-                    elif (n+1)%(nfile/10)==0:
-                        print('{} {} bin mocks at {}<z<{} has finished {}%'.format(gal,rscale,Zrange[k],Zrange[k+znum],(n+1)//(nfile/100)))
-                pool.close() 
-                pool.join()
-
-                # stack mono, mono+quad, mono+quad+hexa
-                NGC  = np.array([mockmono[k][0] for k in range(nfile)]).T
-                SGC  = np.array([mockmono[k][1] for k in range(nfile)]).T
-                NGCSGC = np.array([mockmono[k][2] for k in range(nfile)]).T
-
-                # save data as binary table
-                # name of the mock 2pcf and covariance matrix file(function return)
-                cols = []
-                cols.append(fits.Column(name='NGCmocks',format=str(nfile)+'D',array=NGC))
-                cols.append(fits.Column(name='SGCmocks',format=str(nfile)+'D',array=SGC))
-                cols.append(fits.Column(name='NGC+SGCmocks',format=str(nfile)+'D',array=NGCSGC))
-
-                hdulist = fits.BinTableHDU.from_columns(cols)
-                hdulist.header.update(sbins=nbins,nmu=nmu)
-                hdulist.writeto(mockfits+'wp_pi80.fits.gz',overwrite=True)
-                
-                
+            elif (function == 'wp'):
                 # wp with different pi range
-                for pimax in pimaxs[:-1]:
-                    if os.path.exists(mockFITS[-1]+'wp_pi{}.fits.gz'.format(pimax)):
+                for pimax in pimaxs:
+                    if pimin == 0:
+                        wptailpi = 'wp_pi{}.fits.gz'.format(pimax)
+                    else:
+                        wptailpi = 'wp_pi{}-{}.fits.gz'.format(pimin,pimax)
+                    if os.path.exists(mockFITS[-1]+wptailpi):
                         pass    
                     else: 
                         if mocktype == 'sys':
@@ -175,7 +148,7 @@ for mocktype in ['sys']:
                             if n==0:
                                 print('mock reading&2PCF calculation start')
                             elif (n+1)%(nfile/10)==0:
-                                print('wp_pi{} of {} {} bin PATCHY mocks at {}<z<{} has finished {}%'.format(pimax,gal,rscale,Zrange[k],Zrange[k+znum],(n+1)//(nfile/100)))
+                                print('wp_pi{}-{} of {} {} bin PATCHY mocks at {}<z<{} has finished {}%'.format(pimin,pimax,gal,rscale,Zrange[k],Zrange[k+znum],(n+1)//(nfile/100)))
                         pool.close() 
                         pool.join()
 
@@ -193,7 +166,7 @@ for mocktype in ['sys']:
 
                         hdulist = fits.BinTableHDU.from_columns(cols)
                         hdulist.header.update(sbins=nbins,nmu=nmu,pimax=pimax)
-                        hdulist.writeto('{}wp_pi{}.fits.gz'.format(mockfits,pimax),overwrite=True)
+                        hdulist.writeto(mockfits+wptailpi,overwrite=True)
                 #""" 
             fin=time.time()
             print('z{}z{} NGC+SGC and covariance calculation finished in {:.6}s'.format(Zrange[k],Zrange[k+znum],-start+fin))
@@ -259,7 +232,7 @@ if os.path.exists(flagwp):
         spec = gridspec.GridSpec(nrows=2,ncols=len(pimaxs), height_ratios=[4, 1], hspace=0.3,wspace=0.2)
         ax = np.empty((2,len(pimaxs)), dtype=type(plt.axes))
         for k,pimax in enumerate(pimaxs):
-            hdu = fits.open('{}nosys_FKP/EZmocks_{}_{}_{}_z{}z{}_wp_pi{}.fits.gz'.format(datapath,'nosys',function,rscale,Zrange[0],Zrange[1],pimax)) #
+            hdu = fits.open('{}nosys_FKP/EZmocks_{}_{}_{}_z{}z{}_{}'.format(datapath,'nosys',function,rscale,Zrange[0],Zrange[1],wptailpi)) #
             mocks = hdu[1].data['NGC+SGCmocks']
             Nmock = mocks.shape[1] 
             hdu.close()
@@ -273,7 +246,7 @@ if os.path.exists(flagwp):
                 ax[j,k] = fig.add_subplot(spec[j,k])
                 ax[j,k].plot(s,(meannosys-values[j])/err[j],color='k', label='nosys_FKP')
                 ax[j,k].fill_between(s,((meannosys-errbarnosys)-values[j])/err[j],((meannosys+errbarnosys)-values[j])/err[j],color='k',alpha=0.2)
-                hdu = fits.open('{}sys{}/EZmocks_{}_{}_{}_z{}z{}_wp_pi{}.fits.gz'.format(datapath,mockweight,'sys',function,rscale,Zrange[0],Zrange[1],pimax)) #
+                hdu = fits.open('{}sys{}/EZmocks_{}_{}_{}_z{}z{}_{}'.format(datapath,mockweight,'sys',function,rscale,Zrange[0],Zrange[1],wptailpi)) 
                 mocks = hdu[1].data['NGC+SGCmocks']
                 Nmock = mocks.shape[1] 
                 hdu.close()
@@ -289,11 +262,14 @@ if os.path.exists(flagwp):
                     ax[j,k].set_ylabel('wp')#('\\xi_{}$'.format(k*2))#
                     plt.legend(loc=1)
                     plt.title('wp $\pi$ in [{},{}]'.format(pimin,pimax)+' Mpc$h^{-1}$,'+' weights: {}'.format(mockweight[1:]))
-                    plt.ylim(5,45)
+                    if pimax == 0:
+                        plt.ylim(5,45)
+                    elif pimax == 5:
+                        plt.ylim(3,30)
                     plt.yscale('log')
                 if (j==1):
                     ax[j,k].set_ylabel('$\Delta$ wp/err')
                     plt.ylim(-3,3)
 
-        plt.savefig('wp_sys-vs-nosys{}.png'.format(mockweight))
+        plt.savefig('wp_sys-vs-nosys{}_pi{}-{}.png'.format(mockweight,pimin,pimax))
         plt.close()
