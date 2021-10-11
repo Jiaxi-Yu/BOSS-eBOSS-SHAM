e66fd4d644ca0d5beafaa713ddf056f3defff394
Jiaxi-Yu <yujxjessy@outlook.com>
2021-10-12 00:36:23 +0200

    kcorrection.py kcorr function for Kcorrection

---
+++ SHAM/raw_scripts/latest/kcorrection.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import kcorrect
+
+def kcorr(z,flux,fluxivar,zrecon=0.55):
+    # the redshift of target; flux and fluxivar in nanomaggie; the reconstructed redhshift
+    kcorrect.load_templates()
+    kcorrect.load_filters()
+    coeffs = kcorrect.fit_nonneg(z,flux*1e-9,fluxivar*1e18)
+    #rm = kcorrect.reconstruct_maggies(coeffs)
+    return kcorrect.reconstruct_maggies(coeffs,redshift=zrecon)[1:]/1e9
+    #return in nanomaggie
