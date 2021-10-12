9f721e48efb8515530f63f827f062e199de50bac
Jiaxi-Yu <yujxjessy@outlook.com>
2021-10-12 20:50:10 +0200

    template reading removed

--- SHAM/raw_scripts/latest/kcorrection.py
+++ SHAM/raw_scripts/latest/kcorrection.py
@@ -3,9 +3,6 @@ import kcorrect
 
 def kcorr(z,flux,fluxivar,zrecon=0.55):
     # the redshift of target; flux and fluxivar in nanomaggie; the reconstructed redhshift
-    kcorrect.load_templates()
-    kcorrect.load_filters()
     coeffs = kcorrect.fit_nonneg(z,flux*1e-9,fluxivar*1e18)
-    #rm = kcorrect.reconstruct_maggies(coeffs)
-    return kcorrect.reconstruct_maggies(coeffs,redshift=zrecon)[1:]/1e-9
     #return in nanomaggie
+    return kcorrect.reconstruct_maggies(coeffs,redshift=zrecon)[1:]/1e-9
