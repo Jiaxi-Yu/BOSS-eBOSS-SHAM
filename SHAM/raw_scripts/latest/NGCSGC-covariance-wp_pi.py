aabf1a0a07c07817d591915a67b4b04fde697707
Jiaxi-Yu <yujxjessy@outlook.com>
2021-10-30 14:12:27 +0200

    add the case for ds=5

--- SHAM/raw_scripts/latest/NGCSGC-covariance-wp_pi.py
+++ SHAM/raw_scripts/latest/NGCSGC-covariance-wp_pi.py
@@ -11,8 +11,11 @@ import glob
 import sys
 import re
 
-binmin=5
-binmax=25
+#binmin=5
+#binmax=25
+binmin=0
+binmax=100
+ds = 5
 gal  = sys.argv[1]
 function  = sys.argv[2] # '2PCF'  'wp'
 if function == '2PCF':
@@ -35,6 +38,8 @@ ver = 'DR12'
 if gal=='CMASS':
     Zrange =  np.array([0.43,0.51,0.57,0.43,\
                         0.51,0.57,0.7, 0.7])
+    #Zrange =  np.array([0.43,0.51,\
+    #                    0.51,0.57])
 elif gal == 'LOWZ':
     Zrange =  np.array([0.2, 0.33,0.2,\
                         0.33,0.43,0.43])
@@ -60,7 +65,8 @@ else:
     mockDIR  = ['/hpcstorage/jiayu/PATCHY/{}/z{}z{}/{}/'.format(gal,Zrange[k],Zrange[k+znum],function) for k in range(znum)]
 
 # output root
-mockFITS  = ['{}{}_{}_z{}z{}_mocks_'.format(datapath,gal,rscale,Zrange[k],Zrange[k+znum]) for k in range(znum)]
+#mockFITS  = ['{}{}_{}_z{}z{}_mocks_'.format(datapath,gal,rscale,Zrange[k],Zrange[k+znum]) for k in range(znum)]
+mockFITS  = ['{}_{}_z{}z{}_mocks_'.format(gal,rscale,Zrange[k],Zrange[k+znum]) for k in range(znum)]
 
 for k,mockdir,mockfits in zip(range(znum),mockDIR,mockFITS): 
     start=time.time()
@@ -81,10 +87,15 @@ for k,mockdir,mockfits in zip(range(znum),mockDIR,mockFITS):
         pairroot[9] = mockdir+function+'_PATCHYmock_'+gal+'_{}_'+ver+'_z'+str(Zrange[k])+'z'+str(Zrange[k+znum])+'_1200.{}'
 
     if function == '2PCF':
-        if os.path.exists(mockfits+'quad.fits.gz'):
+        if ds ==1:
+            mockfile = mockfits+'quad.fits.gz'
+        else:
+            mockfile = mockfits+'quad_bin{}.fits.gz'
+
+        if os.path.exists(mockfile):
             pass
         else:
-            comb_part = partial(FCFCcomb,rfmt = rrfile)
+            comb_part = partial(FCFCcomb,rfmt = rrfile,ds=ds)
             # NGC+SGC for mono, quad, hexadeca-pole
             pool = Pool()     
             for n, temp_array in enumerate(pool.imap(comb_part,pairroot)):
@@ -126,20 +137,24 @@ for k,mockdir,mockfits in zip(range(znum),mockDIR,mockFITS):
                 print(name,' saved')
                 hdulist = fits.BinTableHDU.from_columns(cols)
                 hdulist.header.update(sbins=nbins,nmu=nmu)
-                hdulist.writeto(mockfits+name+'.fits.gz',overwrite=True)
+                if ds ==1:
+                    hdulist.writeto(mockfits+name+'.fits.gz',overwrite=True)
+                else:
+                    hdulist.writeto(mockfits+name+'_bin{}.fits.gz'.format(ds),overwrite=True)
+            if ds ==1:
+                # covR calculation for 5-25Mpc/h & quadrupole
+                hdu = fits.open(mockfits+'quad.fits.gz')
+                for GC in ['NGC','SGC','NGC+SGC']:
+                    mocks = hdu[1].data[GC+'mocks'] 
+                    Nmock = mocks.shape[1] 
+                    Ns = int(mocks.shape[0]/2)
+                    mocks = np.vstack((mocks[binmin:binmax,:],mocks[binmin+Ns:binmax+Ns,:]))
+                    covcut  = np.cov(mocks).astype('float32')
+                    Nbins = len(mocks)
+                    covR  = np.linalg.pinv(covcut)*(Nmock-Nbins-2)/(Nmock-1)
+                    np.savetxt(datapath+'covR-'+gal+'_'+GC+'-s'+str(binmin)+'_'+str(binmax)+'-z'+str(Zrange[k])+'z'+str(Zrange[k+znum])+'-quad.dat',covR)
+                hdu.close()
 
-            # covR calculation for 5-25Mpc/h & quadrupole
-            hdu = fits.open(mockfits+'quad.fits.gz')
-            for GC in ['NGC','SGC','NGC+SGC']:
-                mocks = hdu[1].data[GC+'mocks'] 
-                Nmock = mocks.shape[1] 
-                Ns = int(mocks.shape[0]/2)
-                mocks = np.vstack((mocks[binmin:binmax,:],mocks[binmin+Ns:binmax+Ns,:]))
-                covcut  = np.cov(mocks).astype('float32')
-                Nbins = len(mocks)
-                covR  = np.linalg.pinv(covcut)*(Nmock-Nbins-2)/(Nmock-1)
-                np.savetxt(datapath+'covR-'+gal+'_'+GC+'-s'+str(binmin)+'_'+str(binmax)+'-z'+str(Zrange[k])+'z'+str(Zrange[k+znum])+'-quad.dat',covR)
-            hdu.close()
         fin=time.time()
         print('z{}z{} NGC+SGC and covariance calculation finished in {:.6}s'.format(Zrange[k],Zrange[k+znum],-start+fin))
     elif function == 'wp':
